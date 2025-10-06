44e95d72618dcfa9830eb4ef32181dedca297d7e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 16:42:26 +0700

    Implement addToken method

--- src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
+++ src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
@@ -5,6 +5,15 @@ class AuthenticationRepositoryPostgres extends AuthenticationRepository {
     super();
     this._pool = pool;
   }
+
+  async addToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES ($1)',
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
 }
 
 module.exports = AuthenticationRepositoryPostgres;
