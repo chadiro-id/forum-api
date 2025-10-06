e6049d6cdef3d8b3bf24905dfd6faabcd505d636
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 11:45:08 +0700

    New: authentication repository test

---
+++ src/Domains/authentications/_test/AuthenticationRepository.test.js
@@ -0,0 +1,11 @@
+const AuthenticationRepository = require('../AuthenticationRepository');
+
+describe('AuthenticationRepository interface', () => {
+  it('should throw error when invoke unimplemented method', async () => {
+    const authenticationRepository = new AuthenticationRepository();
+
+    await expect(authenticationRepository.addToken('')).rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(authenticationRepository.checkAvailabilityToken('')).rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(authenticationRepository.deleteToken('')).rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
