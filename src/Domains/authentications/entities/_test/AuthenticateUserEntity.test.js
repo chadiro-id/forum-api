77f8a975204c4458ba6ad69da362366ec6e0fd7e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 12:38:43 +0700

    Add positive scenario

--- src/Domains/authentications/entities/_test/AuthenticateUserEntity.test.js
+++ src/Domains/authentications/entities/_test/AuthenticateUserEntity.test.js
@@ -17,4 +17,17 @@ describe('AuthenticateUserEntity', () => {
 
     expect(() => new AuthenticateUserEntity(payload)).toThrow('AUTHENTICATE_USER_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
   });
+
+  it('should create authentication entity correctly', () => {
+    const payload = {
+      username: 'forumapi',
+      password: '12345',
+    };
+
+    const entity = new AuthenticateUserEntity(payload);
+
+    expect(entity).toBeInstanceOf(AuthenticateUserEntity);
+    expect(entity.username).toEqual(payload.username);
+    expect(entity.password).toEqual(payload.password);
+  });
 });
