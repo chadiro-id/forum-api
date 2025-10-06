98a97d8b1d6a9fbbb14f4f5741cc50d5e120f91c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 12:30:37 +0700

    Defining needed property and implement pyload verification

--- src/Domains/authentications/entities/AuthenticateUserEntity.js
+++ src/Domains/authentications/entities/AuthenticateUserEntity.js
@@ -1,5 +1,18 @@
 class AuthenticateUserEntity {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    this.username = payload.username;
+    this.password = payload.password;
+  }
+
+  _verifyPayload(payload) {
+    const { username, password } = payload;
+
+    if (!username || !password) {
+      throw new Error('AUTHENTICATE_USER_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
 }
 
 module.exports = AuthenticateUserEntity;
