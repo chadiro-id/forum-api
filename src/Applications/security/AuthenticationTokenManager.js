29fd21044545e534aa91df861105bdfcb7f64331
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 16:27:27 +0700

    Add params for decode method

--- src/Applications/security/AuthenticationTokenManager.js
+++ src/Applications/security/AuthenticationTokenManager.js
@@ -11,7 +11,7 @@ class AuthenticationTokenManager {
     throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
   }
 
-  async decodePayload() {
+  async decodePayload(_token) {
     throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
   }
 }
