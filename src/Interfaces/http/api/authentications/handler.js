65b7306f04c57728b46d1504e3fb8220655753fb
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 15:53:47 +0700

    Create method for handling post authentication request

--- src/Interfaces/http/api/authentications/handler.js
+++ src/Interfaces/http/api/authentications/handler.js
@@ -1,5 +1,26 @@
+const AuthenticationsUseCase = require('../../../../Applications/use_case/AuthenticationsUseCase');
+
 class AuthenticationsHandler {
-  constructor() {}
+  constructor(container) {
+    this._container = container;
+  }
+
+  async postAuthenticationHandler(request, h) {
+    const useCase = this._container.getInstance(AuthenticationsUseCase.name);
+
+    const { accessToken, refreshToken } = await useCase.authenticate(request.payload);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
 }
 
 module.exports = AuthenticationsHandler;
