66bbb40937154478b4d945677fb93b45e899265e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 16:33:45 +0700

    New: authentication repository postgres

---
+++ src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
@@ -0,0 +1,10 @@
+const AuthenticationRepository = require('../../Domains/authentications/AuthenticationRepository');
+
+class AuthenticationRepositoryPostgres extends AuthenticationRepository {
+  constructor(pool) {
+    super();
+    this._pool = pool;
+  }
+}
+
+module.exports = AuthenticationRepositoryPostgres;
