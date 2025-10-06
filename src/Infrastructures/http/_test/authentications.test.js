50166c362c47e8dced00ebc254e809b1b25ca2d1
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 20:44:10 +0700

    Add negative testing scenario for post authentications request

--- src/Infrastructures/http/_test/authentications.test.js
+++ src/Infrastructures/http/_test/authentications.test.js
@@ -47,5 +47,103 @@ describe('/authentications endpoint', () => {
       expect(responseJson.data.accessToken).toBeDefined();
       expect(responseJson.data.refreshToken).toBeDefined();
     });
+
+    it('should response 400 if username not found', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'dicoding',
+        password: 'secret',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('username tidak ditemukan');
+    });
+
+    it('should response 401 if password wrong', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'dicoding',
+        password: 'wrong_password',
+      };
+      const server = await createServer(container);
+      // Add user
+      await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          username: 'dicoding',
+          password: 'secret',
+          fullname: 'Dicoding Indonesia',
+        },
+      });
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('kredensial yang Anda masukkan salah');
+    });
+
+    it('should response 400 if login payload not contain needed property', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'dicoding',
+      };
+
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('harus mengirimkan username dan password');
+    });
+
+    it('should response 400 if login payload wrong data type', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 123,
+        password: 'secret',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('username dan password harus string');
+    });
   });
 });
