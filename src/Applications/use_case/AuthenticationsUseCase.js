eebe15c23be6906bc8ae7446cf1fd5108ca13c8c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 14:00:42 +0700

    Defining authenticate user method

--- src/Applications/use_case/AuthenticationsUseCase.js
+++ src/Applications/use_case/AuthenticationsUseCase.js
@@ -1,3 +1,35 @@
-class AuthenticationsUseCase {}
+const AuthenticateUserEntity = require('../../Domains/authentications/entities/AuthenticateUserEntity');
+const NewAuthEntity = require('../../Domains/authentications/entities/NewAuthEntity');
+
+class AuthenticationsUseCase {
+  constructor({
+    userRepository,
+    authenticationRepository,
+    authenticationTokenManager,
+    passwordHash
+  }) {
+    this._userRepository = userRepository;
+    this._authenticationRepository = authenticationRepository;
+    this._authenticationTokenManager = authenticationTokenManager;
+    this._passwordHash = passwordHash;
+  }
+
+  async authenticateUser(payload) {
+    const { username, password } = new AuthenticateUserEntity(payload);
+
+    const encryptedPassword = await this._userRepository.getPasswordByUsername(username);
+    await this._passwordHash.comparePassword(password, encryptedPassword);
+
+    const id = await this._userRepository.getIdByUsername(username);
+
+    const accessToken = await this._authenticationTokenManager.createAccessToken({ username, id });
+    const refreshToken = await this._authenticationTokenManager.createRefreshToken({ username, id });
+
+    const authenticationEntity = new NewAuthEntity({ accessToken, refreshToken });
+    await this._authenticationRepository.addToken(authenticationEntity.refreshToken);
+
+    return authenticationEntity;
+  }
+}
 
 module.exports = AuthenticationsUseCase;
