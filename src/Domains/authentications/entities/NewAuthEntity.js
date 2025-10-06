53770c705c143ba930ce314a003eea74c278da5f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 11:23:03 +0700

    Defining property and payload verification

--- src/Domains/authentications/entities/NewAuthEntity.js
+++ src/Domains/authentications/entities/NewAuthEntity.js
@@ -1,5 +1,18 @@
 class NewAuthEntity {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    this.accessToken = payload.accessToken;
+    this.refreshToken = payload.refreshToken;
+  }
+
+  _verifyPayload(payload) {
+    const { accessToken, refreshToken } = payload;
+
+    if (!accessToken || !refreshToken) {
+      throw new Error('NEW_AUTH.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
 }
 
 module.exports = NewAuthEntity;
