d4108471b7fc04f8c455cfa3d0ae87f9ff76aa49
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 15:46:59 +0700

    Defining routes

--- src/Interfaces/http/api/authentications/routes.js
+++ src/Interfaces/http/api/authentications/routes.js
@@ -1,3 +1,19 @@
-const routes = () => ([]);
+const routes = (handler) => ([
+  {
+    method: 'POST',
+    path: '/authentications',
+    handler: handler.postAuthenticationHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/authentications',
+    handler: handler.putAuthenticationHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/authentications',
+    handler: handler.deleteAuthenticationHandler,
+  },
+]);
 
 module.exports = routes;
