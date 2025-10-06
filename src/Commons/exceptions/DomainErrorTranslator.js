5c2bc3289c85a02821fee36d7caa8d17780c7802
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 21:14:17 +0700

    Translate error from authentications use case

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -13,6 +13,8 @@ DomainErrorTranslator._directories = {
   'REGISTER_USER_ENTITY.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'),
   'AUTHENTICATE_USER_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan username dan password'),
   'AUTHENTICATE_USER_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('username dan password harus string'),
+  'AUTHENTICATIONS_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN': new InvariantError('harus mengirimkan token refresh'),
+  'AUTHENTICATIONS_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('refresh token harus string'),
 };
 
 module.exports = DomainErrorTranslator;
