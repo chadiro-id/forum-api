78dbe2143234cc95ebb69d4987e78daeebe536af
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-06 16:46:37 +0700

    Add testing for check availability token method

--- src/Infrastructures/repository/_test/AuthenticationRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/AuthenticationRepositoryPostgres.test.js
@@ -12,7 +12,6 @@ describe('AuthenticationRepositoryPostgres', () => {
     await pool.end();
   });
 
-
   describe('addToken function', () => {
     it('should add token to database', async () => {
       // Arrange
@@ -26,6 +25,29 @@ describe('AuthenticationRepositoryPostgres', () => {
       const tokens = await AuthenticationsTestHelper.findToken(token);
       expect(tokens).toHaveLength(1);
       expect(tokens[0].token).toBe(token);
+    });
+  });
+
+  describe('checkAvailabilityToken method', () => {
+    it('should throw InvariantError if token not available', async () => {
+      // Arrange
+      const authenticationRepository = new AuthenticationRepositoryPostgres(pool);
+      const token = 'token';
+
+      // Action & Assert
+      await expect(authenticationRepository.checkAvailabilityToken(token))
+        .rejects.toThrow(InvariantError);
+    });
+
+    it('should not throw InvariantError if token available', async () => {
+      // Arrange
+      const authenticationRepository = new AuthenticationRepositoryPostgres(pool);
+      const token = 'token';
+      await AuthenticationsTestHelper.addToken(token);
+
+      // Action & Assert
+      await expect(authenticationRepository.checkAvailabilityToken(token))
+        .resolves.not.toThrow(InvariantError);
     });
   });
 });
