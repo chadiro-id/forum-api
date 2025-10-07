cb09046795308a63ab38858aa8f3187a72a0a04a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-07 13:20:37 +0700

    Add payload verification method

--- src/Domains/threads/entities/DetailThreadEntity.js
+++ src/Domains/threads/entities/DetailThreadEntity.js
@@ -1,5 +1,17 @@
 class DetailThreadEntity {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const {
+      id, title, body, date, username, comments
+    } = payload;
+
+    if (!id || !title || !body || !date || !username || !comments) {
+      throw new Error('DETAIL_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
 }
 
 module.exports = DetailThreadEntity;
