498b598bbc79052ffe734e381ba4676709b674b1
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-07 17:03:20 +0700

    Removing unnecessary testing and add positive scenario testing

--- src/Applications/use_case/_test/ThreadsUseCase.test.js
+++ src/Applications/use_case/_test/ThreadsUseCase.test.js
@@ -1,24 +1,54 @@
 const ThreadsUseCase = require('../ThreadsUseCase');
+const AddedThreadEntity = require('../../../Domains/threads/entities/AddedThreadEntity');
+const UserRepository = require('../../../Domains/users/UserRepository');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const NewThreadEntity = require('../../../Domains/threads/entities/NewThreadEntity');
 
 describe('ThreadsUseCase', () => {
   describe('when addThread method invoked', () => {
-    it('should throw error if the credential id is missing', async () => {
-      const useCase = new ThreadsUseCase({});
+    it('should orchestrating business logic correctly', async () => {
+      const payload = {
+        title: 'Title',
+        body: 'body',
+        owner: 'user-123',
+      };
 
-      await expect(useCase.addThread())
-        .rejects
-        .toThrow('THREADS_USE_CASE.MISSING_CREDENTIAL_ID');
-      await expect(useCase.addThread(null))
-        .rejects
-        .toThrow('THREADS_USE_CASE.MISSING_CREDENTIAL_ID');
-    });
+      const mockAddedThread = new AddedThreadEntity({
+        id: 'thread-123',
+        title: payload.title,
+        owner: payload.owner,
+      });
 
-    it('should throw error if the given credential id is not a string', async () => {
-      const useCase = new ThreadsUseCase({});
+      const mockUserRepository = new UserRepository();
+      const mockThreadRepository = new ThreadRepository();
 
-      await expect(useCase.addThread(123))
-        .rejects
-        .toThrow('THREADS_USE_CASE.CREDENTIAL_ID_MUST_BE_A_STRING');
+      mockUserRepository.verifyUserById = jest.fn()
+        .mockImplementation(() => Promise.resolve());
+      mockThreadRepository.addThread = jest.fn()
+        .mockImplementation(() => Promise.resolve(mockAddedThread));
+
+      const useCase = new ThreadsUseCase({
+        userRepository: mockUserRepository,
+        threadRepository: mockThreadRepository,
+      });
+
+      const addedThread = await useCase.addThread(payload);
+
+      expect(addedThread).toStrictEqual(new AddedThreadEntity({
+        id: 'thread-123',
+        title: payload.title,
+        owner: payload.owner,
+      }));
+
+      expect(mockUserRepository.verifyUserById)
+        .toHaveBeenCalledWith(payload.owner);
+
+      expect(mockThreadRepository.addThread)
+        .toHaveBeenCalledWith(new NewThreadEntity({
+          title: payload.title,
+          body: payload.body,
+          owner: payload.owner,
+        }));
     });
   });
 });
