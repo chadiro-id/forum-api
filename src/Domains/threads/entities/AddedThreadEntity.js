dd984b6113a0b92421dc5b28bd0470b26d8b3ad1
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-07 10:04:04 +0700

    Add payload verification method

--- src/Domains/threads/entities/AddedThreadEntity.js
+++ src/Domains/threads/entities/AddedThreadEntity.js
@@ -1,5 +1,15 @@
 class AddedThreadEntity {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { id, title, owner } = payload;
+
+    if (!id || !title || !owner) {
+      throw new Error('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
 }
 
 module.exports = AddedThreadEntity;
