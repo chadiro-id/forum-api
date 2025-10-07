35583c682114d3dc22d8b0c0ac393ce906b4c038
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-07 13:58:25 +0700

    Create threads method

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -1,3 +1,11 @@
-class ThreadRepository {}
+class ThreadRepository {
+  async addThread(_payload) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getThreadById(_id) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
 
 module.exports = ThreadRepository;
