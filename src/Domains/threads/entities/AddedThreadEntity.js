8ea8b8a9dcc37e483053ab522316d6da6143009a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-07 10:25:11 +0700

    Verify payload property data type

--- src/Domains/threads/entities/AddedThreadEntity.js
+++ src/Domains/threads/entities/AddedThreadEntity.js
@@ -9,6 +9,10 @@ class AddedThreadEntity {
     if (!id || !title || !owner) {
       throw new Error('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
     }
+
+    if (typeof id !== 'string' || typeof title !== 'string' || typeof owner !== 'string') {
+      throw new Error('ADDED_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
   }
 }
 
