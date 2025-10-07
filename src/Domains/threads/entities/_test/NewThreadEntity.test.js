c676f591374d3790fbfd38ee37a0ce65963586b4
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-07 09:04:54 +0700

    Add testing for property data type

--- src/Domains/threads/entities/_test/NewThreadEntity.test.js
+++ src/Domains/threads/entities/_test/NewThreadEntity.test.js
@@ -8,4 +8,12 @@ describe('NewThreadEntity', () => {
     expect(() => new NewThreadEntity(missingTitlePayload)).toThrow('NEW_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
     expect(() => new NewThreadEntity(missingBodyPayload)).toThrow('NEW_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
   });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const titleNotStringPayload = { title: 123, body: 'Thread Body' };
+    const bodyNotStringPayload = { title: 'Thread Title', body: [1, 2, 3] };
+
+    expect(() => new NewThreadEntity(titleNotStringPayload)).toThrow('NEW_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    expect(() => new NewThreadEntity(bodyNotStringPayload)).toThrow('NEW_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
 });
