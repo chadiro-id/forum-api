722670f84968dfb41a364d5ebe2c0e9f9002f440
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-08 18:25:36 +0700

    Update test scenario to be more descriptive

--- src/Domains/authentications/_test/AuthenticationRepository.test.js
+++ src/Domains/authentications/_test/AuthenticationRepository.test.js
@@ -1,11 +1,16 @@
 const AuthenticationRepository = require('../AuthenticationRepository');
 
-describe('AuthenticationRepository interface', () => {
-  it('should throw error when invoke unimplemented method', async () => {
-    const authenticationRepository = new AuthenticationRepository();
+describe('AuthenticationRepository', () => {
+  describe('when abstract method invoked', () => {
+    it('should throw error with properly message', async () => {
+      const authenticationRepository = new AuthenticationRepository();
 
-    await expect(authenticationRepository.addToken('')).rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(authenticationRepository.checkAvailabilityToken('')).rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(authenticationRepository.deleteToken('')).rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+      await expect(authenticationRepository.addToken(''))
+        .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+      await expect(authenticationRepository.checkAvailabilityToken(''))
+        .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+      await expect(authenticationRepository.deleteToken(''))
+        .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    });
   });
 });
