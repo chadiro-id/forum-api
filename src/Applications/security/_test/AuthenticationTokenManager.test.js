a16ebabe84d60bf4dcb3519cc75f82fe86093416
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-08 19:22:02 +0700

    Update scenario to be more descriptive

--- src/Applications/security/_test/AuthenticationTokenManager.test.js
+++ src/Applications/security/_test/AuthenticationTokenManager.test.js
@@ -1,12 +1,18 @@
 const AuthenticationTokenManager = require('../AuthenticationTokenManager');
 
-describe('AuthenticationTokenManager interface', () => {
-  it('should throw error when invoke unimplemented method', async () => {
-    const tokenManager = new AuthenticationTokenManager();
+describe('AuthenticationTokenManager', () => {
+  describe('when abstract method invoked', () => {
+    it('should throw error with properly message', async () => {
+      const tokenManager = new AuthenticationTokenManager();
 
-    await expect(tokenManager.createAccessToken('')).rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-    await expect(tokenManager.createRefreshToken('')).rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-    await expect(tokenManager.verifyRefreshToken('')).rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-    await expect(tokenManager.decodePayload('')).rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+      await expect(tokenManager.createAccessToken(''))
+        .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+      await expect(tokenManager.createRefreshToken(''))
+        .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+      await expect(tokenManager.verifyRefreshToken(''))
+        .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+      await expect(tokenManager.decodePayload(''))
+        .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+    });
   });
 });
--- src/Applications/security/_test/PasswordHash.test.js
+++ src/Applications/security/_test/PasswordHash.test.js
@@ -1,10 +1,14 @@
 const PasswordHash = require('../PasswordHash');
 
-describe('PasswordHash interface', () => {
-  it('should throw error when invoke abstract behavior', async () => {
-    const passwordHash = new PasswordHash();
+describe('PasswordHash', () => {
+  describe('when abstract method invoked', () => {
+    it('should throw error with properly message', async () => {
+      const passwordHash = new PasswordHash();
 
-    await expect(passwordHash.hash('dummy_password')).rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
-    await expect(passwordHash.comparePassword('plain', 'encrypted')).rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+      await expect(passwordHash.hash('dummy_password'))
+        .rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+      await expect(passwordHash.comparePassword('plain', 'encrypted'))
+        .rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+    });
   });
 });
