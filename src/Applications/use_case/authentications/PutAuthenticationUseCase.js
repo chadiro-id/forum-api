327776a6d8d5ad1d4715748d84cb1cebadfbb4f4
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-08 10:06:53 +0700

    Create payload verification

--- src/Applications/use_case/authentications/PutAuthenticationUseCase.js
+++ src/Applications/use_case/authentications/PutAuthenticationUseCase.js
@@ -1,5 +1,23 @@
 class PutAuthenticationUseCase {
-  constructor() {}
+  constructor({
+    authenticationRepository,
+    authenticationTokenManager,
+  }) {
+    this._authenticationRepository = authenticationRepository;
+    this._authenticationTokenManager = authenticationTokenManager;
+  }
+
+  async execute(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { refreshToken } = payload;
+
+    if (!refreshToken) {
+      throw new Error('PUT_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN');
+    }
+  }
 }
 
 module.exports = PutAuthenticationUseCase;
