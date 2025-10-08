86cf06e10393e89845c53cda6b7e34c49446c3f8
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-08 12:45:54 +0700

    Registering authentications use cases dependencies

--- src/Infrastructures/containers/useCaseContainer.js
+++ src/Infrastructures/containers/useCaseContainer.js
@@ -1,5 +1,10 @@
+const AuthenticationTokenManager = require('../../Applications/security/AuthenticationTokenManager');
 const PasswordHash = require('../../Applications/security/PasswordHash');
+const AddAuthenticationUseCase = require('../../Applications/use_case/authentications/AddAuthenticationUseCase');
+const DeleteAuthenticationUseCase = require('../../Applications/use_case/authentications/DeleteAuthenticationUseCase');
+const PutAuthenticationUseCase = require('../../Applications/use_case/authentications/PutAuthenticationUseCase');
 const AddUserUseCase = require('../../Applications/use_case/users/AddUserUseCase');
+const AuthenticationRepository = require('../../Domains/authentications/AuthenticationRepository');
 const UserRepository = require('../../Domains/users/UserRepository');
 
 const setup = (container) => {
@@ -17,6 +22,64 @@ const setup = (container) => {
           {
             name: 'passwordHash',
             internal: PasswordHash.name,
+          },
+        ],
+      },
+    },
+  ]);
+
+  container.register([
+    {
+      key: AddAuthenticationUseCase.name,
+      Class: AddAuthenticationUseCase,
+      parameter: {
+        injectType: 'destructuring',
+        dependencies: [
+          {
+            name: 'userRepository',
+            internal: UserRepository.name,
+          },
+          {
+            name: 'authenticationRepository',
+            internal: AuthenticationRepository.name,
+          },
+          {
+            name: 'authenticationTokenManager',
+            internal: AuthenticationTokenManager.name,
+          },
+          {
+            name: 'passwordHash',
+            internal: PasswordHash.name,
+          },
+        ],
+      },
+    },
+    {
+      key: PutAuthenticationUseCase.name,
+      Class: PutAuthenticationUseCase,
+      parameter: {
+        injectType: 'destructuring',
+        dependencies: [
+          {
+            name: 'authenticationRepository',
+            internal: AuthenticationRepository.name,
+          },
+          {
+            name: 'authenticationTokenManager',
+            internal: AuthenticationTokenManager.name,
+          },
+        ],
+      },
+    },
+    {
+      key: DeleteAuthenticationUseCase.name,
+      Class: DeleteAuthenticationUseCase,
+      parameter: {
+        injectType: 'destructuring',
+        dependencies: [
+          {
+            name: 'authenticationRepository',
+            internal: AuthenticationRepository.name,
           },
         ],
       },
