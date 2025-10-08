b838dff3df0a16d972dd8534c525952a9a45b042
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-08 16:33:40 +0700

    Update test to be more descriptive

--- src/Domains/threads/entities/_test/AddedThreadEntity.test.js
+++ src/Domains/threads/entities/_test/AddedThreadEntity.test.js
@@ -1,57 +1,80 @@
 const AddedThreadEntity = require('../AddedThreadEntity');
 
 describe('AddedThreadEntity', () => {
-  const correctPayload = {
-    id: 'id',
+  const exampleValidPayload = {
+    id: 'thread-123',
     title: 'title',
-    owner: 'owner'
+    owner: 'owner',
   };
 
-  describe('when the given payload is wrong', () => {
-    it('should throw error if payload not contain required property', () => {
-      const missingId = { ...correctPayload };
+  describe('when the given payload is not valid', () => {
+    it('should throw error if payload not contain needed property', () => {
+      const missingId = { ...exampleValidPayload };
       delete missingId.id;
 
-      const missingTitle = { ...correctPayload };
+      const missingTitle = { ...exampleValidPayload };
       delete missingTitle.title;
 
-      const missingOwner = { ...correctPayload };
+      const missingOwner = { ...exampleValidPayload };
       delete missingOwner.owner;
 
-      const emptyId = { ...correctPayload, id: '' };
-      const emptyTitle = { ...correctPayload, title: '' };
-      const emptyOwner = { ...correctPayload, owner: '' };
+      const emptyId = { ...exampleValidPayload, id: '' };
+      const emptyTitle = { ...exampleValidPayload, title: '' };
+      const emptyOwner = { ...exampleValidPayload, owner: '' };
 
-      expect(() => new AddedThreadEntity(missingId)).toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
-      expect(() => new AddedThreadEntity(missingTitle)).toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
-      expect(() => new AddedThreadEntity(missingOwner)).toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
-      expect(() => new AddedThreadEntity(emptyId)).toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
-      expect(() => new AddedThreadEntity(emptyTitle)).toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
-      expect(() => new AddedThreadEntity(emptyOwner)).toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThreadEntity(missingId))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThreadEntity(missingTitle))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThreadEntity(missingOwner))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThreadEntity(emptyId))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThreadEntity(emptyTitle))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThreadEntity(emptyOwner))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
     });
 
     it('should throw error if payload property does not meet data type spesification', () => {
-      const invalidIdType = { ...correctPayload, id: [1, 2, 3] };
-      const invalidTitleType = { ...correctPayload, title: 123 };
-      const invalidOwnerType = { ...correctPayload, owner: true };
+      const idNotString = {
+        ...exampleValidPayload,
+        id: [1, 2, 3],
+      };
+      const titleNotString = {
+        ...exampleValidPayload,
+        title: 123,
+      };
+      const ownerNotString = {
+        ...exampleValidPayload,
+        owner: true,
+      };
 
-      expect(() => new AddedThreadEntity(invalidIdType)).toThrow('ADDED_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
-      expect(() => new AddedThreadEntity(invalidTitleType)).toThrow('ADDED_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
-      expect(() => new AddedThreadEntity(invalidOwnerType)).toThrow('ADDED_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+      expect(() => new AddedThreadEntity(idNotString))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+      expect(() => new AddedThreadEntity(titleNotString))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+      expect(() => new AddedThreadEntity(ownerNotString))
+        .toThrow('ADDED_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
     });
   });
 
-  describe('when the given payload is correct', () => {
+  describe('when the given payload is valid', () => {
     it('should create entity correctly', () => {
-      const { id, title, owner } = new AddedThreadEntity(correctPayload);
+      const payload = { ...exampleValidPayload };
 
-      expect(id).toEqual(correctPayload.id);
-      expect(title).toEqual(correctPayload.title);
-      expect(owner).toEqual(correctPayload.owner);
+      const { id, title, owner } = new AddedThreadEntity(payload);
+
+      expect(id).toEqual(payload.id);
+      expect(title).toEqual(payload.title);
+      expect(owner).toEqual(payload.owner);
     });
 
     it('should create entity correctly and not contain extra properties', () => {
-      const extraPayload = { ...correctPayload, extra: 'something' };
+      const extraPayload = {
+        ...exampleValidPayload,
+        extra: 'extra property',
+      };
 
       const addedThread = new AddedThreadEntity(extraPayload);
 
@@ -60,7 +83,6 @@ describe('AddedThreadEntity', () => {
         title: extraPayload.title,
         owner: extraPayload.owner,
       });
-
       expect(addedThread.extra).toBeUndefined();
     });
   });
