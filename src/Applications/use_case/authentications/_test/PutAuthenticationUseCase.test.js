e763f0338cfe0e5017b65e60fcc6b043b590c99c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-08 10:46:33 +0700

    Add positive scenerio testing

--- src/Applications/use_case/authentications/_test/PutAuthenticationUseCase.test.js
+++ src/Applications/use_case/authentications/_test/PutAuthenticationUseCase.test.js
@@ -1,3 +1,5 @@
+const AuthenticationRepository = require('../../../../Domains/authentications/AuthenticationRepository');
+const AuthenticationTokenManager = require('../../../security/AuthenticationTokenManager');
 const PutAuthenticationUseCase = require('../PutAuthenticationUseCase');
 
 describe('PutAuthenticationUseCase', () => {
@@ -30,6 +32,43 @@ describe('PutAuthenticationUseCase', () => {
 
       await expect(putAuthenticationUseCase.execute(useCasePayload))
         .rejects.toThrow('PUT_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN');
+    });
+  });
+
+  describe('when executed with correct payload', () => {
+    it('should orchestrating the put authentication action correctly', async () => {
+      const useCasePayload = {
+        refreshToken: 'some_refresh_token',
+      };
+
+      const mockAuthenticationRepository = new AuthenticationRepository();
+      const mockAuthenticationTokenManager = new AuthenticationTokenManager();
+
+      mockAuthenticationTokenManager.verifyRefreshToken = jest.fn()
+        .mockImplementation(() => Promise.resolve());
+      mockAuthenticationRepository.checkAvailabilityToken = jest.fn()
+        .mockImplementation(() => Promise.resolve());
+      mockAuthenticationTokenManager.decodePayload = jest.fn()
+        .mockImplementation(() => Promise.resolve({ username: 'forumapi', id: 'user-123' }));
+      mockAuthenticationTokenManager.createAccessToken = jest.fn()
+        .mockImplementation(() => Promise.resolve('some_new_access_token'));
+
+      const putAuthenticationUseCase = new PutAuthenticationUseCase({
+        authenticationRepository: mockAuthenticationRepository,
+        authenticationTokenManager: mockAuthenticationTokenManager,
+      });
+
+      const accessToken = await putAuthenticationUseCase.execute(useCasePayload);
+
+      expect(mockAuthenticationTokenManager.verifyRefreshToken)
+        .toHaveBeenCalledWith(useCasePayload.refreshToken);
+      expect(mockAuthenticationRepository.checkAvailabilityToken)
+        .toHaveBeenCalledWith(useCasePayload.refreshToken);
+      expect(mockAuthenticationTokenManager.decodePayload)
+        .toHaveBeenCalledWith(useCasePayload.refreshToken);
+      expect(mockAuthenticationTokenManager.createAccessToken)
+        .toHaveBeenCalledWith({ username: 'forumapi', id: 'user-123' });
+      expect(accessToken).toEqual('some_new_access_token');
     });
   });
 });
