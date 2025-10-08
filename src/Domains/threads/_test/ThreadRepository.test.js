567683e1f0bcd8893e468fa7a95eefc310dba8d0
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-08 17:43:19 +0700

    Update test scenario to be more descriptive

--- src/Domains/threads/_test/ThreadRepository.test.js
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -1,12 +1,14 @@
 const ThreadRepository = require('../ThreadRepository');
 
 describe('ThreadRepository', () => {
-  describe('when unimplemented method invoked', () => {
-    it('should throw error', async () => {
+  describe('when abstract method invoked', () => {
+    it('should throw error with properly message', async () => {
       const threadRepository = new ThreadRepository();
 
-      await expect(threadRepository.addThread({})).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-      await expect(threadRepository.getThreadById('')).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+      await expect(threadRepository.addThread({}))
+        .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+      await expect(threadRepository.getThreadById(''))
+        .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     });
   });
 });
