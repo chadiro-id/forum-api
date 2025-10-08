1f26a097731d7f3fefa5fc8d7f446a1d76aa3a40
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-08 12:44:38 +0700

    Update error message translations

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -7,14 +7,16 @@ const DomainErrorTranslator = {
 };
 
 DomainErrorTranslator._directories = {
-  'REGISTER_USER_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'),
-  'REGISTER_USER_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'),
+  'REGISTER_USER_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'),
+  'REGISTER_USER_ENTITY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'),
   'REGISTER_USER_ENTITY.USERNAME_EXCEED_CHAR_LIMIT': new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'),
   'REGISTER_USER_ENTITY.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'),
-  'AUTHENTICATE_USER_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan username dan password'),
-  'AUTHENTICATE_USER_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('username dan password harus string'),
-  'AUTHENTICATIONS_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN': new InvariantError('harus mengirimkan token refresh'),
-  'AUTHENTICATIONS_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('refresh token harus string'),
+  'USER_LOGIN_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan username dan password'),
+  'USER_LOGIN_ENTITY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('username dan password harus string'),
+  'PUT_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN': new InvariantError('harus mengirimkan token refresh'),
+  'PUT_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('refresh token harus string'),
+  'DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN': new InvariantError('harus mengirimkan token refresh'),
+  'DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('refresh token harus string'),
 };
 
 module.exports = DomainErrorTranslator;
--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -3,21 +3,25 @@ const InvariantError = require('../InvariantError');
 
 describe('DomainErrorTranslator', () => {
   it('should translate error correctly', () => {
-    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY')))
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'));
-    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'));
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.USERNAME_EXCEED_CHAR_LIMIT')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'));
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'));
-    expect(DomainErrorTranslator.translate(new Error('AUTHENTICATE_USER_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY')))
+    expect(DomainErrorTranslator.translate(new Error('USER_LOGIN_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY')))
       .toStrictEqual(new InvariantError('harus mengirimkan username dan password'));
-    expect(DomainErrorTranslator.translate(new Error('AUTHENTICATE_USER_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+    expect(DomainErrorTranslator.translate(new Error('USER_LOGIN_ENTITY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
       .toStrictEqual(new InvariantError('username dan password harus string'));
-    expect(DomainErrorTranslator.translate(new Error('AUTHENTICATIONS_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN')))
+    expect(DomainErrorTranslator.translate(new Error('PUT_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN')))
       .toStrictEqual(new InvariantError('harus mengirimkan token refresh'));
-    expect(DomainErrorTranslator.translate(new Error('AUTHENTICATIONS_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
+    expect(DomainErrorTranslator.translate(new Error('PUT_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
+      .toStrictEqual(new InvariantError('refresh token harus string'));
+    expect(DomainErrorTranslator.translate(new Error('DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN')))
+      .toStrictEqual(new InvariantError('harus mengirimkan token refresh'));
+    expect(DomainErrorTranslator.translate(new Error('DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
       .toStrictEqual(new InvariantError('refresh token harus string'));
   });
 
