057851876f0407b23ae55540c3c73f728e6f77c3
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 20:42:09 +0700

    Describing test more concise

--- src/Applications/security/_test/PasswordHash.test.js
+++ src/Applications/security/_test/PasswordHash.test.js
@@ -1,14 +1,12 @@
 const PasswordHash = require('../PasswordHash');
 
 describe('PasswordHash', () => {
-  describe('when abstract method invoked', () => {
-    it('should throw error with properly message', async () => {
-      const passwordHash = new PasswordHash();
+  it('should throw error with properly message when abstract method invoked', async () => {
+    const passwordHash = new PasswordHash();
 
-      await expect(passwordHash.hash('dummy_password'))
-        .rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
-      await expect(passwordHash.comparePassword('plain', 'encrypted'))
-        .rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
-    });
+    await expect(passwordHash.hash('dummy_password'))
+      .rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+    await expect(passwordHash.comparePassword('plain', 'encrypted'))
+      .rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
   });
 });
