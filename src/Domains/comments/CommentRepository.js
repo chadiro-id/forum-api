0769ecade05fa47117eb764357e05b50108ca5ed
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 00:28:08 +0700

    Defining the needed methods

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -1,3 +1,15 @@
-class CommentRepository {}
+class CommentRepository {
+  async addComment(_userId, _content) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getCommentById(_id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteCommentById(_id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
 
 module.exports = CommentRepository;
