55d9fb68c3f7b234988adedd87b082ab8b562a7c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 21:05:56 +0700

    Update scenario to be more concise

--- src/Domains/authentications/_test/AuthenticationRepository.test.js
+++ src/Domains/authentications/_test/AuthenticationRepository.test.js
@@ -1,16 +1,14 @@
 const AuthenticationRepository = require('../AuthenticationRepository');
 
 describe('AuthenticationRepository', () => {
-  describe('when abstract method invoked', () => {
-    it('should throw error with properly message', async () => {
-      const authenticationRepository = new AuthenticationRepository();
+  it('should throw error with properly message when abstract method invoked', async () => {
+    const authenticationRepository = new AuthenticationRepository();
 
-      await expect(authenticationRepository.addToken(''))
-        .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-      await expect(authenticationRepository.checkAvailabilityToken(''))
-        .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-      await expect(authenticationRepository.deleteToken(''))
-        .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    });
+    await expect(authenticationRepository.addToken(''))
+      .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(authenticationRepository.checkAvailabilityToken(''))
+      .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(authenticationRepository.deleteToken(''))
+      .rejects.toThrow('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
--- src/Domains/threads/_test/ThreadRepository.test.js
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -1,14 +1,12 @@
 const ThreadRepository = require('../ThreadRepository');
 
 describe('ThreadRepository', () => {
-  describe('when abstract method invoked', () => {
-    it('should throw error with properly message', async () => {
-      const threadRepository = new ThreadRepository();
+  it('should throw error with properly message', async () => {
+    const threadRepository = new ThreadRepository();
 
-      await expect(threadRepository.addThread({}))
-        .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-      await expect(threadRepository.getThreadById(''))
-        .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    });
+    await expect(threadRepository.addThread({}))
+      .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(threadRepository.getThreadById(''))
+      .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
