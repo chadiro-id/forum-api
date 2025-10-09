e01d703db344583ec197b9b835c9d31d808ebdc2
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 01:58:21 +0700

    Add argument verification

--- src/Applications/use_case/comments/AddCommentUseCase.js
+++ src/Applications/use_case/comments/AddCommentUseCase.js
@@ -1,5 +1,21 @@
 class AddCommentUseCase {
-  constructor() {}
+  constructor(commentRepository) {
+    this._commentRepository = commentRepository;
+  }
+
+  async execute(userId, _payload) {
+    this._verifyUserId(userId);
+  }
+
+  _verifyUserId(id) {
+    if (!id) {
+      throw new Error('ADD_COMMENT_USE_CASE.MISSING_USER_ID');
+    }
+
+    if (typeof id !== 'string') {
+      throw new Error('ADD_COMMENT_USE_CASE.USER_ID_MUST_BE_A_STRING');
+    }
+  }
 }
 
 module.exports = AddCommentUseCase;
