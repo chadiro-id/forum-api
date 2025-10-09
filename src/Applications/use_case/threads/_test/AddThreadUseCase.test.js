264f498d8c684861ac0082308d6ff63e6ad0705d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 22:11:17 +0700

    Fix: use case test as its needed params has been changed

--- src/Applications/use_case/threads/_test/AddThreadUseCase.test.js
+++ src/Applications/use_case/threads/_test/AddThreadUseCase.test.js
@@ -5,50 +5,48 @@ const ThreadRepository = require('../../../../Domains/threads/ThreadRepository')
 const NewThreadEntity = require('../../../../Domains/threads/entities/NewThreadEntity');
 
 describe('AddThreadUseCase', () => {
-  describe('when executed', () => {
-    it('should orchestrating the add thread action correctly', async () => {
-      const payload = {
-        title: 'Title',
-        body: 'body',
-        owner: 'user-123',
-      };
+  it('should orchestrating the add thread action correctly', async () => {
+    const useCaseArg1 = 'user-123';
+    const useCaseArg2 = {
+      title: 'Title',
+      body: 'body',
+    };
 
-      const mockAddedThread = new AddedThreadEntity({
-        id: 'thread-123',
-        title: payload.title,
-        owner: payload.owner,
-      });
-
-      const mockUserRepository = new UserRepository();
-      const mockThreadRepository = new ThreadRepository();
-
-      mockUserRepository.verifyUserById = jest.fn()
-        .mockImplementation(() => Promise.resolve());
-      mockThreadRepository.addThread = jest.fn()
-        .mockImplementation(() => Promise.resolve(mockAddedThread));
-
-      const addThreadUseCase = new AddThreadUseCase({
-        userRepository: mockUserRepository,
-        threadRepository: mockThreadRepository,
-      });
-
-      const addedThread = await addThreadUseCase.execute(payload);
-
-      expect(addedThread).toStrictEqual(new AddedThreadEntity({
-        id: 'thread-123',
-        title: payload.title,
-        owner: payload.owner,
-      }));
-
-      expect(mockUserRepository.verifyUserById)
-        .toHaveBeenCalledWith(payload.owner);
-
-      expect(mockThreadRepository.addThread)
-        .toHaveBeenCalledWith(new NewThreadEntity({
-          title: payload.title,
-          body: payload.body,
-          owner: payload.owner,
-        }));
+    const mockAddedThread = new AddedThreadEntity({
+      id: 'thread-123',
+      title: useCaseArg2.title,
+      owner: useCaseArg1,
     });
+
+    const mockUserRepository = new UserRepository();
+    const mockThreadRepository = new ThreadRepository();
+
+    mockUserRepository.verifyUserById = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockThreadRepository.addThread = jest.fn()
+      .mockImplementation(() => Promise.resolve(mockAddedThread));
+
+    const addThreadUseCase = new AddThreadUseCase({
+      userRepository: mockUserRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    const addedThread = await addThreadUseCase.execute(useCaseArg1, useCaseArg2);
+
+    expect(addedThread).toStrictEqual(new AddedThreadEntity({
+      id: 'thread-123',
+      title: useCaseArg2.title,
+      owner: useCaseArg1,
+    }));
+
+    expect(mockUserRepository.verifyUserById)
+      .toHaveBeenCalledWith(useCaseArg1);
+
+    expect(mockThreadRepository.addThread)
+      .toHaveBeenCalledWith(new NewThreadEntity({
+        title: useCaseArg2.title,
+        body: useCaseArg2.body,
+        userId: useCaseArg1,
+      }));
   });
 });
