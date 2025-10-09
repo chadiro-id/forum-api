64d4a3fe00344d5cfea00f6906f72610b004c741
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 18:01:06 +0700

    Create handler method for post thread request

--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -1,6 +1,27 @@
+const AddThreadUseCase = require('../../../../Applications/use_case/threads/AddThreadUseCase');
+
 class ThreadsHandler {
   constructor(container) {
     this._container = container;
+
+    this.postThreadHandler = this.postThreadHandler.bind(this);
+  }
+
+  async postThreadHandler(request, h) {
+    const { id: credentialId } = request.auth.credentials;
+
+    const addThreadUseCase = this._container.getInstance(AddThreadUseCase.name);
+    const addedThread = await addThreadUseCase.execute(credentialId, request.payload);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedThread,
+      },
+    });
+
+    response.code(201);
+    return response;
   }
 }
 
