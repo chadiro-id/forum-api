6be60853b530c27b850f90b20d7a4e3b95dd5bbe
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 20:40:09 +0700

    Modify test scenario to be more concise

--- src/Applications/security/_test/AuthenticationTokenManager.test.js
+++ src/Applications/security/_test/AuthenticationTokenManager.test.js
@@ -1,18 +1,16 @@
 const AuthenticationTokenManager = require('../AuthenticationTokenManager');
 
 describe('AuthenticationTokenManager', () => {
-  describe('when abstract method invoked', () => {
-    it('should throw error with properly message', async () => {
-      const tokenManager = new AuthenticationTokenManager();
+  it('should throw error with properly message when abstract method invoked', async () => {
+    const tokenManager = new AuthenticationTokenManager();
 
-      await expect(tokenManager.createAccessToken(''))
-        .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-      await expect(tokenManager.createRefreshToken(''))
-        .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-      await expect(tokenManager.verifyRefreshToken(''))
-        .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-      await expect(tokenManager.decodePayload(''))
-        .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-    });
+    await expect(tokenManager.createAccessToken(''))
+      .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+    await expect(tokenManager.createRefreshToken(''))
+      .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+    await expect(tokenManager.verifyRefreshToken(''))
+      .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+    await expect(tokenManager.decodePayload(''))
+      .rejects.toThrow('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
   });
 });
