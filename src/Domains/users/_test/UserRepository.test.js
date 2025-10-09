11f74926014192a28439151b89a198ec27034821
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 20:11:42 +0700

    Assert verify user by id method

--- src/Domains/users/_test/UserRepository.test.js
+++ src/Domains/users/_test/UserRepository.test.js
@@ -1,18 +1,18 @@
 const UserRepository = require('../UserRepository');
 
 describe('UserRepository', () => {
-  describe('when abstract method invoked', () => {
-    it('should throw error with properly message', async () => {
-      const userRepository = new UserRepository();
+  it('should throw error with properly message when abstract method invoked', async () => {
+    const userRepository = new UserRepository();
 
-      await expect(userRepository.addUser({}))
-        .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-      await expect(userRepository.verifyAvailableUsername(''))
-        .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-      await expect(userRepository.getPasswordByUsername(''))
-        .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-      await expect(userRepository.getIdByUsername(''))
-        .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    });
+    await expect(userRepository.addUser({}))
+      .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.verifyAvailableUsername(''))
+      .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.verifyUserById(''))
+      .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.getPasswordByUsername(''))
+      .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.getIdByUsername(''))
+      .rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
