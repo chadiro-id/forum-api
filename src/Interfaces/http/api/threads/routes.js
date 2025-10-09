787ac9c4ba99bc4afe6772575a81ec0e25c195f3
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 18:05:46 +0700

    Defining post threads route

--- src/Interfaces/http/api/threads/routes.js
+++ src/Interfaces/http/api/threads/routes.js
@@ -1,3 +1,12 @@
-const routes = () => [];
+const routes = (handler) => ([
+  {
+    method: 'POST',
+    path: '/threads',
+    handler: handler.postThreadHandler,
+    options: {
+      auth: 'forumapi_jwt',
+    },
+  },
+]);
 
 module.exports = routes;
