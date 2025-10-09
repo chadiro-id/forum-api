149349bb6fc343abdc9c320f17f011283bd82781
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 18:29:28 +0700

    Rename owner property to userId

--- src/Domains/threads/entities/NewThreadEntity.js
+++ src/Domains/threads/entities/NewThreadEntity.js
@@ -4,17 +4,17 @@ class NewThreadEntity {
 
     this.title = payload.title;
     this.body = payload.body;
-    this.owner = payload.owner;
+    this.userId = payload.userId;
   }
 
   _verifyPayload(payload) {
-    const { title, body, owner } = payload;
+    const { title, body, userId } = payload;
 
-    if (!title || !body || !owner) {
+    if (!title || !body || !userId) {
       throw new Error('NEW_THREAD_ENTITY.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
-    if (typeof title !== 'string' || typeof body !== 'string' || typeof owner !== 'string') {
+    if (typeof title !== 'string' || typeof body !== 'string' || typeof userId !== 'string') {
       throw new Error('NEW_THREAD_ENTITY.NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
   }
