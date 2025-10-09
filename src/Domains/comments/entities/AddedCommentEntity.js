fd18bedfd4e42451936de3834dfbb24b3080fa02
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-09 23:19:13 +0700

    Provide payload verification

--- src/Domains/comments/entities/AddedCommentEntity.js
+++ src/Domains/comments/entities/AddedCommentEntity.js
@@ -1,5 +1,15 @@
 class AddedCommentEntity {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { id, content, owner } = payload;
+
+    if (!id || !content || !owner) {
+      throw new Error('ADDED_COMMENT_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
 }
 
 module.exports = AddedCommentEntity;
