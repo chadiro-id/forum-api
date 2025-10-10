ae8bb4b5d93660c5b871dbe6f7e97deb48af9496
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 09:04:04 +0700

    Create comment repository postgres class

---
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -0,0 +1,31 @@
+const CommentRepository = require('../../Domains/comments/CommentRepository');
+const AddedCommentEntity = require('../../Domains/comments/entities/AddedCommentEntity');
+
+class CommentRepositoryPostgres extends CommentRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async addComment(entity) {
+    const { threadId, content, ownerId } = entity;
+
+    const id = `comment-${this._idGenerator()}`;
+
+    const query = {
+      text: 'INSERT INTO comments (id, content, thread_id, owner_id) VALUES ($1, $2, $3, $4) RETURNING id, content, owner_id',
+      values: [id, content, threadId, ownerId],
+    };
+
+    const result = await this._pool.query(query);
+
+    return new AddedCommentEntity({
+      id: result.rows[0].id,
+      content: result.rows[0].content,
+      owner: result.rows[0].owner_id,
+    });
+  }
+}
+
+module.exports = CommentRepositoryPostgres;
