cf3c72712979ecb288d0d0972d06013bc1299e3d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 22:50:18 +0700

    Configure business logic

--- src/Applications/use_case/threads/GetDetailThreadUseCase.js
+++ src/Applications/use_case/threads/GetDetailThreadUseCase.js
@@ -1,5 +1,19 @@
 class GetDetailThreadUseCase {
-  constructor() {}
+  constructor({
+    threadRepository,
+    commentRepository,
+  }) {
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentRepository;
+  }
+
+  async execute(threadId) {
+    const detailThread = await this._threadRepository.getDetailThread(threadId);
+    const threadComments = await this._commentRepository.getCommentsByThreadId(threadId);
+
+    detailThread.comments = threadComments;
+    return detailThread;
+  }
 }
 
 module.exports = GetDetailThreadUseCase;
