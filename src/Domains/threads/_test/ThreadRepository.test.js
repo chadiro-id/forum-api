a5cd1d1950dd63d6fca0daa4625645d36e33f0ad
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 11:53:40 +0700

    Rename get detail thread methods and add thread exists verification method

--- src/Domains/threads/_test/ThreadRepository.test.js
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -1,12 +1,14 @@
 const ThreadRepository = require('../ThreadRepository');
 
 describe('ThreadRepository', () => {
-  it('should throw error with properly message', async () => {
+  it('should throw error with properly message when abstract method invoked', async () => {
     const threadRepository = new ThreadRepository();
 
     await expect(threadRepository.addThread({}))
       .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(threadRepository.getDetailThreadById(''))
+    await expect(threadRepository.getDetailThread(''))
+      .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(threadRepository.verifyThreadExists(''))
       .rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
