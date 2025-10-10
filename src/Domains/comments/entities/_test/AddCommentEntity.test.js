b1558bb2b7efc7eddb4361938497978b4ae0bdd8
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 08:08:03 +0700

    New: add comment entity

---
+++ src/Domains/comments/entities/_test/AddCommentEntity.test.js
@@ -0,0 +1,39 @@
+const AddCommentEntity = require('../AddCommentEntity');
+
+describe('AddCommentEntity', () => {
+  const dummyPayload = {
+    threadId: 'thread-123',
+    content: 'some content',
+    ownerId: 'user-123',
+  };
+
+  describe('Bad payload', () => {
+    it('should throw error when payload not contain needed property', () => {
+      const missingThreadId = { ...dummyPayload };
+      delete missingThreadId.threadId;
+      const missingContent = { ...dummyPayload };
+      delete missingContent.content;
+      const missingOwnerId = { ...dummyPayload };
+      delete missingOwnerId.ownerId;
+
+      const emptyThreadId = { ...dummyPayload, threadId: '' };
+      const emptyContent = { ...dummyPayload, content: '' };
+      const emptyOwnerId = { ...dummyPayload, ownerId: '' };
+
+      const expectedError = new Error('ADD_COMMENT_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+
+      expect(new AddCommentEntity(missingThreadId))
+        .toThrow(expectedError);
+      expect(new AddCommentEntity(missingContent))
+        .toThrow(expectedError);
+      expect(new AddCommentEntity(missingOwnerId))
+        .toThrow(expectedError);
+      expect(new AddCommentEntity(emptyThreadId))
+        .toThrow(expectedError);
+      expect(new AddCommentEntity(emptyContent))
+        .toThrow(expectedError);
+      expect(new AddCommentEntity(emptyOwnerId))
+        .toThrow(expectedError);
+    });
+  });
+});
