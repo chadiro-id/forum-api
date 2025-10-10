5be06eb0d5d288408a5183326c7a3eef4937b919
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 14:12:06 +0700

    Create method for handling post comment request

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -1,6 +1,28 @@
+const AddCommentUseCase = require('../../../../Applications/use_case/comments/AddCommentUseCase');
+
 class CommentsHandler {
   constructor(container) {
     this._container = container;
+
+    this.postCommentHandler = this.postCommentHandler.bind(this);
+  }
+
+  async postCommentHandler(request, h) {
+    const { threadId } = request.params;
+    const { id: userId } = request.auth.credentials;
+
+    const addCommentUseCase = this._container.getInstance(AddCommentUseCase.name);
+    const addedComment = await addCommentUseCase.execute({ ...request.payload, threadId, userId });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedComment,
+      },
+    });
+
+    response.code(201);
+    return response;
   }
 }
 
