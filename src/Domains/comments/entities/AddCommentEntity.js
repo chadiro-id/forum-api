7c0fe1522ac0a87bb52b83bbc2fddc16315af7ac
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 08:17:03 +0700

    Create payload verifications

--- src/Domains/comments/entities/AddCommentEntity.js
+++ src/Domains/comments/entities/AddCommentEntity.js
@@ -1,5 +1,15 @@
 class AddCommentEntity {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { threadId, content, ownerId } = payload;
+
+    if (!threadId || !content || !ownerId) {
+      throw new Error('ADD_COMMENT_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
 }
 
 module.exports = AddCommentEntity;
