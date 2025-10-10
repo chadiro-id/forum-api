1de24af3e4fcec2e9499d85119637968eda4ec7d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-10 15:43:30 +0700

    Define business logic for deleteing comment

--- src/Applications/use_case/comments/DeleteCommentUseCase.js
+++ src/Applications/use_case/comments/DeleteCommentUseCase.js
@@ -1,5 +1,19 @@
 class DeleteCommentUseCase {
-  constructor() {}
+  constructor({
+    threadRepository,
+    commentRepository,
+  }) {
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentRepository;
+  }
+
+  async execute(payload) {
+    const { threadId, commentId } = payload;
+
+    await this._threadRepository.verifyThreadExists(threadId);
+    await this._commentRepository.verifyCommentExists(commentId);
+    await this._commentRepository.deleteCommentById(commentId);
+  }
 }
 
 module.exports = DeleteCommentUseCase;
