b08bb61707ad32fbba8155e966fd410724683650
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-11 12:37:36 +0700

    Defining properties and payload verification

--- src/Domains/replies/entities/NewReply.js
+++ src/Domains/replies/entities/NewReply.js
@@ -1,5 +1,19 @@
 class NewReply {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    this.commentId = payload.commentId;
+    this.content = payload.content;
+    this.ownerId = payload.ownerId;
+  }
+
+  _verifyPayload(payload) {
+    const { commentId, content, ownerId } = payload;
+
+    if (!commentId || !content || !ownerId) {
+      throw new Error('NEW_REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
 }
 
 module.exports = NewReply;
