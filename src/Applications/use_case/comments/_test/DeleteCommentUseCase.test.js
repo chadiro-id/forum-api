bcf17b7cd9c5c009ea65fab18b540166e366c738
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-11 23:55:22 +0700

    Fix: stub use case constructor

--- src/Applications/use_case/comments/_test/DeleteCommentUseCase.test.js
+++ src/Applications/use_case/comments/_test/DeleteCommentUseCase.test.js
@@ -3,7 +3,7 @@ const DeleteCommentUseCase = require('../DeleteCommentUseCase');
 describe('DeleteCommentUseCase', () => {
   describe('fails execution', () => {
     it('should throw error when the given payload is bad', async () => {
-      const deleteCommentUseCase = new DeleteCommentUseCase();
+      const deleteCommentUseCase = new DeleteCommentUseCase({});
 
       await expect(deleteCommentUseCase.execute())
         .rejects.toThrow();
