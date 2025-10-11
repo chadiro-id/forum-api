b3cae8366d497b52d0e88fc6d832e651af496879
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-11 11:54:11 +0700

    New: reply repository unit test

---
+++ src/Domains/replies/_test/ReplyRepository.test.js
@@ -0,0 +1,16 @@
+const ReplyRepository = require('../ReplyRepository');
+
+describe('ReplyRepository', () => {
+  it('should throw error with properly message when abstract method invoked', () => {
+    const replyRepository = new ReplyRepository();
+
+    expect(replyRepository.addReply({}))
+      .toThrow('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    expect(replyRepository.getRepliesByCommentIds([]))
+      .toThrow('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    expect(replyRepository.deleteReplyById(''))
+      .toThrow('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    expect(replyRepository.verifyExistsById(''))
+      .toThrow('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
