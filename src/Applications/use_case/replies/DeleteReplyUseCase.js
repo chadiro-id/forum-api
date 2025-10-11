636202e806278fafd4530d92c9f6cb8b755191aa
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-11 16:34:38 +0700

    New: delete reply use case

---
+++ src/Applications/use_case/replies/DeleteReplyUseCase.js
@@ -0,0 +1,24 @@
+class DeleteReplyUseCase {
+  constructor({
+    threadRepository,
+    commentRepository,
+    replyRepository,
+  }) {
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentRepository;
+    this._replyRepository = replyRepository;
+  }
+
+  async execute(payload) {
+    const {
+      threadId, commentId, replyId, userId
+    } = payload;
+
+    await this._threadRepository.verifyThreadExists(threadId);
+    await this._commentRepository.verifyCommentExists(commentId);
+    await this._replyRepository.verifyReplyOwner(replyId, userId);
+    await this._replyRepository.deleteReplyById(replyId);
+  }
+}
+
+module.exports = DeleteReplyUseCase;
