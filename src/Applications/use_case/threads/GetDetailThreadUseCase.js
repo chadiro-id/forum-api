aaa38cec79ad0a41804a036850318d47470998a9
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-11 07:11:19 +0700

    Mapping detail thread

--- src/Applications/use_case/threads/GetDetailThreadUseCase.js
+++ src/Applications/use_case/threads/GetDetailThreadUseCase.js
@@ -8,10 +8,24 @@ class GetDetailThreadUseCase {
   }
 
   async execute(threadId) {
-    const detailThread = await this._threadRepository.getDetailThread(threadId);
+    const thread = await this._threadRepository.getDetailThread(threadId);
     const threadComments = await this._commentRepository.getCommentsByThreadId(threadId);
 
-    detailThread.comments = threadComments;
+    const detailThread = { ...thread, date: thread.created_at };
+    delete detailThread.created_at;
+
+    detailThread.comments = threadComments.map(({
+      id, content, username, created_at, is_delete
+    }) => {
+      const mappedContent = is_delete ? '**komentar telah dihapus**' : content;
+      return {
+        id,
+        username,
+        date: created_at,
+        content: mappedContent,
+      };
+    });
+
     return detailThread;
   }
 }
