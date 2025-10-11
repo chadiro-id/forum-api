7ce77228ec25ba8941e5f9ed0f8f342e187efdb1
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-11 13:49:41 +0700

    Create handler for post reply request

--- src/Interfaces/http/api/replies/handler.js
+++ src/Interfaces/http/api/replies/handler.js
@@ -1,6 +1,28 @@
+const AddReplyUseCase = require('../../../../Applications/use_case/replies/AddReplyUseCase');
+
 class RepliesHandler {
   constructor(container) {
     this._container = container;
+
+    this.postReplyHandler = this.postReplyHandler.bind(this);
+  }
+
+  async postReplyHandler(request, h) {
+    const { threadId, commentId } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    const addReplyUseCase = this._container.getInstance(AddReplyUseCase.name);
+    const addedReply = await addReplyUseCase.execute({ ...request.payload, threadId, commentId, userId: credentialId });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedReply,
+      }
+    });
+
+    response.code(201);
+    return response;
   }
 }
 
