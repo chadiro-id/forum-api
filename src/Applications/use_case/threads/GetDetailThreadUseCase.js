a98f94eb5721e20d43c594a34a7bc0e2a6c121fa
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-11 19:46:31 +0700

    Add comment replies into detail thread

--- src/Applications/use_case/threads/GetDetailThreadUseCase.js
+++ src/Applications/use_case/threads/GetDetailThreadUseCase.js
@@ -2,31 +2,55 @@ class GetDetailThreadUseCase {
   constructor({
     threadRepository,
     commentRepository,
+    replyRepository,
   }) {
     this._threadRepository = threadRepository;
     this._commentRepository = commentRepository;
+    this._replyRepository = replyRepository;
   }
 
   async execute(threadId) {
     const thread = await this._threadRepository.getDetailThread(threadId);
-    const threadComments = await this._commentRepository.getCommentsByThreadId(threadId);
+    const comments = await this._commentRepository.getCommentsByThreadId(threadId);
+
+    const commentIds = comments.map(({ id }) => id);
+
+    const replies = await this._replyRepository.getRepliesByCommentIds(commentIds);
+    const repliesGrouped = this._groupRepliesByCommentId(replies);
 
     const detailThread = { ...thread, date: thread.created_at };
     delete detailThread.created_at;
 
-    detailThread.comments = threadComments.map(({
+    detailThread.comments = comments.map(({
       id, content, username, created_at, is_delete
-    }) => {
-      const mappedContent = is_delete ? '**komentar telah dihapus**' : content;
-      return {
-        id,
-        username,
-        date: created_at,
-        content: mappedContent,
-      };
-    });
+    }) => ({
+      id,
+      username,
+      date: created_at,
+      content: is_delete ? '**komentar telah dihapus**' : content,
+      replies: repliesGrouped[id] || [],
+    }));
 
     return detailThread;
+  }
+
+  _groupRepliesByCommentId(repliesArray) {
+    return repliesArray.reduce((acc, reply) => {
+      const commentId = reply.comment_id;
+
+      if (!acc[commentId]) {
+        acc[commentId] = [];
+      }
+
+      acc[commentId].push({
+        id: reply.id,
+        content: reply.is_delete ? '**balasan telah dihapus**' : reply.content,
+        date: reply.created_at,
+        username: reply.username,
+      });
+
+      return acc;
+    }, {});
   }
 }
 
