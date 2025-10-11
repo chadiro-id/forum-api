ce49c37f6c5d48815b92baf7d599972b9dca3a3f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-11 13:37:49 +0700

    Create add reply method

--- src/Infrastructures/repository/ReplyRepositoryPostgres.js
+++ src/Infrastructures/repository/ReplyRepositoryPostgres.js
@@ -7,6 +7,24 @@ class ReplyRepositoryPostgres extends ReplyRepository {
     this._pool = pool;
     this._idGenerator = idGenerator;
   }
+
+  async addReply(newReply) {
+    const { commentId, content, ownerId } = newReply;
+
+    const id = `reply-${this._idGenerator()}`;
+
+    const query = {
+      text: 'INSERT INTO replies (id, content, comment_id, owner_id) VALUES ($1, $2, $3, $4) RETURNING id, content, owner_id',
+      values: [id, content, commentId, ownerId],
+    };
+
+    const result = await this._pool.query(query);
+    return {
+      id: result.rows[0].id,
+      content: result.rows[0].content,
+      owner: result.rows[0].owner_id,
+    };
+  }
 }
 
 module.exports = ReplyRepositoryPostgres;
