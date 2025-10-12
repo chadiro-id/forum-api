25260f7e4fee5d3c28f8440cd06c55853ea97b19
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 16:46:34 +0700

    New: added thread entity unit test

---
+++ src/Domains/threads/entities/_test/AddedThread.test.js
@@ -0,0 +1,35 @@
+const AddedThread = require('../AddedThread');
+
+describe('AddedThread entity', () => {
+  const dummyPayload = {
+    id: 'thread-123',
+    title: 'Something thread title',
+    owner: 'user-123'
+  };
+
+  describe('Payload is bad', () => {
+    const missingId = { ...dummyPayload };
+    delete missingId.id;
+    const missingTitle = { ...dummyPayload };
+    delete missingTitle.title;
+    const missingOwner = { ...dummyPayload };
+    delete missingOwner.owner;
+
+    const emptyId = { ...dummyPayload, id: '' };
+    const emptyTitle = { ...dummyPayload, title: '' };
+    const emptyOwner = { ...dummyPayload, owner: '' };
+
+    expect(() => new AddedThread(missingId))
+      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new AddedThread(missingTitle))
+      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new AddedThread(missingOwner))
+      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new AddedThread(emptyId))
+      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new AddedThread(emptyTitle))
+      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new AddedThread(emptyOwner))
+      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+});
