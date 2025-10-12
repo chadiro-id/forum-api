a9239509db7f6a1aeaab6a623443aea31d171ffb
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 16:49:38 +0700

    Provide payload validation

--- src/Domains/threads/entities/AddedThread.js
+++ src/Domains/threads/entities/AddedThread.js
@@ -1,5 +1,15 @@
 class AddedThread {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { id, title, owner } = payload;
+
+    if (!id || !title || !owner) {
+      throw new Error('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+  }
 }
 
 module.exports = AddedThread;
