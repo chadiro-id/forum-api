c4c8560ee9cbe03dd615d7ca86e28062344f9a13
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 20:54:39 +0700

    Provide payload validations

--- src/Domains/replies/entities/AddedReply.js
+++ src/Domains/replies/entities/AddedReply.js
@@ -1,5 +1,23 @@
 class AddedReply {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { id, content, owner } = payload;
+
+    if (!id || !content || !owner) {
+      throw new Error('ADDED_REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof id !== 'string'
+      || typeof content !== 'string'
+      || typeof owner !== 'string'
+    ) {
+      throw new Error('ADDED_REPLY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
 }
 
 module.exports = AddedReply;
