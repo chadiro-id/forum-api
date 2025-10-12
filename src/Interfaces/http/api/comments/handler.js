cad3eee43ddcc54cac9afd13fafcfe5be5b579c1
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-13 02:52:17 +0700

    Update post comment handler

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -1,5 +1,7 @@
 const AddCommentUseCase = require('../../../../Applications/use_case/comments/AddCommentUseCase');
 const DeleteCommentUseCase = require('../../../../Applications/use_case/comments/DeleteCommentUseCase');
+const InvariantError = require('../../../../Commons/exceptions/InvariantError');
+const NewComment = require('../../../../Domains/comments/entities/NewComment');
 
 class CommentsHandler {
   constructor(container) {
@@ -11,10 +13,20 @@ class CommentsHandler {
 
   async postCommentHandler(request, h) {
     const { threadId } = request.params;
-    const { id: userId } = request.auth.credentials;
+    const { id: owner } = request.auth.credentials;
+
+    const { content } = request.payload;
+
+    if (!content || typeof content !== 'string') {
+      throw new InvariantError('Komentar harus di isi dengan benar');
+    }
+
+    const newComment = new NewComment({
+      threadId, owner, content
+    });
 
     const addCommentUseCase = this._container.getInstance(AddCommentUseCase.name);
-    const addedComment = await addCommentUseCase.execute({ ...request.payload, threadId, userId });
+    const addedComment = await addCommentUseCase.execute(newComment);
 
     const response = h.response({
       status: 'success',
