3de390c5f9ca439f73d2f44aa4b987df0ac7085a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 19:45:36 +0700

    Provide payload validation

--- src/Domains/comments/entities/NewComment.js
+++ src/Domains/comments/entities/NewComment.js
@@ -1,5 +1,23 @@
 class NewComment {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { threadId, content, owner } = payload;
+
+    if (!threadId || !content || !owner) {
+      throw new Error('NEW_COMMENT.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof threadId !== 'string'
+      || typeof content !== 'string'
+      || typeof owner !== 'string'
+    ) {
+      throw new Error('NEW_COMMENT.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
 }
 
 module.exports = NewComment;
