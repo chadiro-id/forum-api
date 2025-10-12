ae4597ed490788129e81e1763a5d83d6b31f2951
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 15:25:14 +0700

    Provide payload properties validation

--- src/Domains/threads/entities/NewThread.js
+++ src/Domains/threads/entities/NewThread.js
@@ -1,6 +1,22 @@
 class NewThread {
-  constructor() {
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
 
+  _verifyPayload(payload) {
+    const { title, body, owner } = payload;
+
+    if (!title || !body || !owner) {
+      throw new Error('NEW_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof title !== 'string'
+      || typeof body !== 'string'
+      || typeof owner !== 'string'
+    ) {
+      throw new Error('NEW_THREAD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
   }
 }
 
