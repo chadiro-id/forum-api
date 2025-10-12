ec90de0d01df4cbd1166b2a1814294283c45d439
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 12:04:43 +0700

    Modify column order and clean up comments

--- database/migrations/1759992396661_create-table-replies.js
+++ database/migrations/1759992396661_create-table-replies.js
@@ -1,21 +1,8 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
 exports.up = (pgm) => {
   pgm.createTable('replies', {
     id: {
       type: 'VARCHAR(50)',
       primaryKey: true,
-    },
-    content: {
-      type: 'TEXT',
-      notNull: true,
     },
     comment_id: {
       type: 'VARCHAR(50)',
@@ -23,6 +10,10 @@ exports.up = (pgm) => {
     },
     owner_id: {
       type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    content: {
+      type: 'TEXT',
       notNull: true,
     },
     is_delete: {
@@ -54,11 +45,6 @@ exports.up = (pgm) => {
   });
 };
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
 exports.down = (pgm) => {
   pgm.dropConstraint('replies', 'replies_owner_id_fkey', { ifExists: true });
   pgm.dropConstraint('replies', 'replies_comment_id_fkey', { ifExists: true });
