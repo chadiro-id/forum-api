63ba10cc6a96610aae1c8dac13bc084b592718da
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 16:53:05 +0700

    Fix: it block

--- src/Domains/threads/entities/_test/AddedThread.test.js
+++ src/Domains/threads/entities/_test/AddedThread.test.js
@@ -8,28 +8,30 @@ describe('AddedThread entity', () => {
   };
 
   describe('Payload is bad', () => {
-    const missingId = { ...dummyPayload };
-    delete missingId.id;
-    const missingTitle = { ...dummyPayload };
-    delete missingTitle.title;
-    const missingOwner = { ...dummyPayload };
-    delete missingOwner.owner;
+    it('should throw error when payload not contain needed property', () => {
+      const missingId = { ...dummyPayload };
+      delete missingId.id;
+      const missingTitle = { ...dummyPayload };
+      delete missingTitle.title;
+      const missingOwner = { ...dummyPayload };
+      delete missingOwner.owner;
 
-    const emptyId = { ...dummyPayload, id: '' };
-    const emptyTitle = { ...dummyPayload, title: '' };
-    const emptyOwner = { ...dummyPayload, owner: '' };
+      const emptyId = { ...dummyPayload, id: '' };
+      const emptyTitle = { ...dummyPayload, title: '' };
+      const emptyOwner = { ...dummyPayload, owner: '' };
 
-    expect(() => new AddedThread(missingId))
-      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-    expect(() => new AddedThread(missingTitle))
-      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-    expect(() => new AddedThread(missingOwner))
-      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-    expect(() => new AddedThread(emptyId))
-      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-    expect(() => new AddedThread(emptyTitle))
-      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-    expect(() => new AddedThread(emptyOwner))
-      .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThread(missingId))
+        .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThread(missingTitle))
+        .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThread(missingOwner))
+        .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThread(emptyId))
+        .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThread(emptyTitle))
+        .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new AddedThread(emptyOwner))
+        .toThrow('ADDED_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    });
   });
 });
