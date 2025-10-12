904b893be8c5eef956ba5320bcd97e859d8c570e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 20:12:15 +0700

    Provide payload validation

--- src/Domains/comments/entities/AddedComment.js
+++ src/Domains/comments/entities/AddedComment.js
@@ -1,5 +1,23 @@
 class AddedComment {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { id, content, owner } = payload;
+
+    if (!id || !content || !owner) {
+      throw new Error('ADDED_COMMENT.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof id !== 'string'
+      || typeof content !== 'string'
+      || typeof owner !== 'string'
+    ) {
+      throw new Error('ADDED_COMMENT.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
 }
 
 module.exports = AddedComment;
