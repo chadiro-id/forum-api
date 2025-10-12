50b68bacca7799f3c307bcd89bd1d42c77b82e6f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-13 02:40:08 +0700

    Describe positive scenario success full execution

--- src/Applications/use_case/comments/_test/AddCommentUseCase.test.js
+++ src/Applications/use_case/comments/_test/AddCommentUseCase.test.js
@@ -1,8 +1,12 @@
+const CommentRepository = require('../../../../Domains/comments/CommentRepository');
+const AddedComment = require('../../../../Domains/comments/entities/AddedComment');
+const NewComment = require('../../../../Domains/comments/entities/NewComment');
+const ThreadRepository = require('../../../../Domains/threads/ThreadRepository');
 const AddCommentUseCase = require('../AddCommentUseCase');
 
 describe('AddCommentUseCase', () => {
-  describe('fails execution', () => {
-    it('should throw error when the given payload is bad', async () => {
+  describe('Fails execution', () => {
+    it('should throw error when the given payload is not an instance of NewComment', async () => {
       const addCommentUseCase = new AddCommentUseCase({});
 
       await expect(addCommentUseCase.execute())
@@ -11,6 +15,44 @@ describe('AddCommentUseCase', () => {
         .rejects.toThrow();
       await expect(addCommentUseCase.execute({}))
         .rejects.toThrow();
+    });
+  });
+
+  describe('Successfull execution', () => {
+    it('should correctly orchestrating the add comment action', async () => {
+      const payload = new NewComment({
+        threadId: 'thread-123',
+        content: 'Sebuah komentar',
+        owner: 'user-123',
+      });
+
+      const mockThreadRepository = new ThreadRepository();
+      const mockCommentRepository = new CommentRepository();
+      mockThreadRepository.verifyThreadExists = jest.fn()
+        .mockImplementation(() => Promise.resolve());;
+      mockCommentRepository.addComment = jest.fn()
+        .mockImplementation(() => Promise.resolve('comment-123'));
+
+      const addCommentUseCase = new AddCommentUseCase({
+        threadRepository: mockThreadRepository,
+        commentRepository: mockCommentRepository,
+      });
+
+      const addedComment = await addCommentUseCase.execute(payload);
+
+      expect(mockThreadRepository.verifyThreadExists).toHaveBeenCalledTimes(1);
+      expect(mockCommentRepository.addComment).toHaveBeenCalledTimes(1);
+      expect(mockCommentRepository.addComment).toHaveBeenCalledWith({
+        thread_id: 'thread-123',
+        owner_id: 'user-123',
+        content: 'Sebuah komentar',
+      });
+
+      expect(addedComment).toEqual(new AddedComment({
+        id: 'comment-123',
+        content: 'Sebuah komentar',
+        owner: 'user-123',
+      }));
     });
   });
 });
