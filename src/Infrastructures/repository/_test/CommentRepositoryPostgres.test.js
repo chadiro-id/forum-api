d979f0663290b2715779e07ed041192859847cc5
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-12 10:51:00 +0700

    Describe method implementations test

--- src/Infrastructures/repository/_test/CommentRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/CommentRepositoryPostgres.test.js
@@ -4,9 +4,63 @@ const CommentRepositoryPostgres = require('../CommentRepositoryPostgres');
 describe('CommentRepositoryPostgres', () => {
   describe('CommentRepository contract enforcement', () => {
     it('must be an instance of CommentRepository', () => {
-      const commentRepositoryPostgres = new CommentRepositoryPostgres();
+      const commentRepositoryPostgres = new CommentRepositoryPostgres({}, () => '');
 
       expect(commentRepositoryPostgres).toBeInstanceOf(CommentRepository);
+    });
+  });
+
+  describe('Method implementations', () => {
+    let mockPool;
+    let fakeIdGenerator;
+    let commentRepositoryPostgres;
+
+    const dummyNewComment = {
+      threadId: 'thread-123',
+      ownerId: 'user-456',
+      content: 'Sebuah komentar',
+    };
+
+    beforeEach(() => {
+      mockPool = {
+        query: jest.fn(),
+      };
+      fakeIdGenerator = () => '123';
+
+      commentRepositoryPostgres = new CommentRepositoryPostgres(mockPool, fakeIdGenerator);
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    describe('addComment', () => {
+      it('should persist the comment and return the added comment', async () => {
+        mockPool.query.mockResolvedValue({
+          rows: [{ id: 'comment-123', content: 'Sebuah komentar', owner_id: 'user-456' }],
+          rowCount: 1,
+        });
+
+        const addedComment = await commentRepositoryPostgres.addComment(dummyNewComment);
+
+        expect(mockPool.query).toHaveBeenCalledTimes(1);
+        expect(mockPool.query).toHaveBeenCalledWith(
+          expect.objectContaining({
+            text: expect.stringContaining('INSERT INTO comments'),
+          })
+        );
+        const calledValues = mockPool.query.mock.calls[0][0].values;
+        expect(calledValues[0]).toEqual('comment-123');
+        expect(calledValues[1]).toEqual(dummyNewComment.content);
+        expect(calledValues[2]).toEqual(dummyNewComment.threadId);
+        expect(calledValues[3]).toEqual(dummyNewComment.ownerId);
+
+        expect(addedComment).toEqual({
+          id: 'comment-123',
+          content: 'Sebuah komentar',
+          owner: 'user-456',
+        });
+      });
     });
   });
 });
