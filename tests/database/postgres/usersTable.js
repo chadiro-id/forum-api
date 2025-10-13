585a758d78ec6ec59f223f59890b2e531ad6c79e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-13 19:41:26 +0700

    Change class based into function

--- tests/database/postgres/usersTable.js
+++ tests/database/postgres/usersTable.js
@@ -1,32 +1,33 @@
 /* istanbul ignore file */
-class UsersTable {
-  constructor(pool) {
-    this._pool = pool;
-  }
-  async addUser({
+const tableHelper = (pool) => {
+  const addUser = async ({
     id = 'user-123', username = 'forumapi', password = 'supersecret^_^@01', fullname = 'Forum Api',
-  }) {
+  }) => {
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4)',
       values: [id, username, password, fullname],
     };
 
-    await this._pool.query(query);
-  }
+    await pool.query(query);
+  };
 
-  async findUsersById(id) {
+  const findUsersById = async (id) => {
     const query = {
       text: 'SELECT * FROM users WHERE id = $1',
       values: [id],
     };
 
-    const result = await this._pool.query(query);
+    const result = await pool.query(query);
     return result.rows;
-  }
+  };
 
-  async cleanTable() {
-    await this._pool.query('DELETE FROM users WHERE 1=1');
-  }
+  const clean = async () => {
+    await pool.query('DELETE FROM users WHERE 1=1');
+  };
+
+  return {
+    addUser, findUsersById, clean
+  };
 };
 
-module.exports = UsersTable;
+module.exports = tableHelper;
