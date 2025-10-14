5be65c011c1570eba7a7c2ffc902aa09609ab685
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-14 17:48:08 +0700

    Provide add comment table test helper

--- tests/db_helper/postgres/comments.js
+++ tests/db_helper/postgres/comments.js
@@ -1,9 +1,25 @@
 const comments = (pool) => {
+  const add = async ({
+    id = 'comment-123',
+    threadId = 'thread-123',
+    owner = 'user-123',
+    content = 'Sebuah komentar',
+  }) => {
+    const query = {
+      text: 'INSERT INTO comments (id, thread_id, owner_id, content) VALUES ($1, $2, $3, $4) RETURNING id',
+      values: [id, threadId, owner, content],
+    };
+
+    await pool.query(query);
+    return id;
+  };
+
   const clean = async () => {
     await pool.query('DELETE FROM comments WHERE 1=1');
   };
 
   return {
+    add,
     clean,
   };
 };
