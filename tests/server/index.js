a62cddbb035d8fa5396f21415716876b9702760c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-14 14:21:24 +0700

    New: server test helper

---
+++ tests/server/index.js
@@ -0,0 +1,46 @@
+/* istanbul ignore file */
+const { createServer } = require('../../src/Infrastructures/http/server');
+const container = require('../../src/Infrastructures/containers/container');
+
+let server;
+let initialized;
+
+const inject = async (method, url, headers, payload) => {
+  return server.inject({
+    method,
+    url,
+    headers,
+    payload,
+  });
+};
+
+exports.init = async () => {
+  if (initialized) return;
+
+  if (!server) {
+    server = await createServer(container);
+  }
+
+  await server.initialize();
+};
+
+exports.stop = async () => {
+  initialized = false;
+  await server.stop();
+};
+
+exports.post = async (endpoint, options = {}) => {
+  return inject('POST', endpoint, options.headers, options.payload);
+};
+
+exports.get = async (endpoint, options = {}) => {
+  return inject('GET', endpoint, options.headers);
+};
+
+exports.put = async (endpoint, options = {}) => {
+  return inject('PUT', endpoint, options.headers, options.payload);
+};
+
+exports.delete = async (enpoint, options = {}) => {
+  return inject('DELETE', enpoint, options.headers, options.payload);
+};
