3efc377bc222f6a23de7cb106cd32dc7f8a3c5b3
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-15 12:36:43 +0700

    New: reply entity

---
+++ src/Domains/replies/entities/Reply.js
@@ -0,0 +1,5 @@
+class Reply {
+  constructor() {}
+}
+
+module.exports = Reply;
---
+++ src/Domains/replies/entities/_test/Reply.test.js
@@ -0,0 +1,61 @@
+const Reply = require('../Reply');
+
+describe('Reply Entity', () => {
+  const dummyPayload = {
+    id: 'reply-123',
+    content: 'Sebuah balasan',
+    date: '2025-10-15T02:08:54.384Z',
+    username: 'superuser'
+  };
+
+  describe('Bad payload', () => {
+    it('should throw error when payload not contain needed property', () => {
+      const missingId = { ...dummyPayload };
+      delete missingId.id;
+      const missingContent = { ...dummyPayload };
+      delete missingContent.content;
+      const missingDate = { ...dummyPayload };
+      delete missingDate.date;
+      const missingUsername = { ...dummyPayload };
+      delete missingUsername.username;
+
+      const emptyId = { ...dummyPayload, id: '' };
+      const emptyContent = { ...dummyPayload, content: '' };
+      const emptyDate = { ...dummyPayload, date: '' };
+      const emptyUsername = { ...dummyPayload, username: '' };
+
+      expect(() => new Reply(missingId))
+        .toThrow('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new Reply(missingContent))
+        .toThrow('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new Reply(missingDate))
+        .toThrow('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new Reply(missingUsername))
+        .toThrow('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new Reply(emptyId))
+        .toThrow('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new Reply(emptyContent))
+        .toThrow('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new Reply(emptyDate))
+        .toThrow('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+      expect(() => new Reply(emptyUsername))
+        .toThrow('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    });
+
+    it('should throw error when payload property does not meet data type specification', () => {
+      const idNotString = { ...dummyPayload, id: 123 };
+      const contentNotString = { ...dummyPayload, content: ['Balasan'] };
+      const dateNotString = { ...dummyPayload, date: 2025 };
+      const usernameNotString = { ...dummyPayload, username: true };
+
+      expect(() => new Reply(idNotString))
+        .toThrow('REPLY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+      expect(() => new Reply(contentNotString))
+        .toThrow('REPLY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+      expect(() => new Reply(dateNotString))
+        .toThrow('REPLY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+      expect(() => new Reply(usernameNotString))
+        .toThrow('REPLY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+  });
+});
