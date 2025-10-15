5dc22688b96312a7c7e0c4f022661da550d350d2
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-15 12:45:17 +0700

    Provide payload validation

--- src/Domains/replies/entities/Reply.js
+++ src/Domains/replies/entities/Reply.js
@@ -1,5 +1,28 @@
 class Reply {
-  constructor() {}
+  constructor(payload) {
+    this._validatePayload(payload);
+  }
+
+  _validatePayload(payload) {
+    const { id, content, date, username } = payload;
+
+    if (!id || !content || !date || !username) {
+      throw new Error('REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof id !== 'string'
+      || typeof content !== 'string'
+      || typeof date !== 'string'
+      || typeof username !== 'string'
+    ) {
+      throw new Error('REPLY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+
+    if (Number.isNaN(Date.parse(date))) {
+      throw new Error('REPLY.INVALID_DATE_STRING');
+    }
+  }
 }
 
 module.exports = Reply;
