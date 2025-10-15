85f1a24137d04919376c7cef136cf2be482dac86
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-15 19:12:18 +0700

    Update error message

--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -3,21 +3,21 @@ const InvariantError = require('../InvariantError');
 
 describe('DomainErrorTranslator', () => {
   it('should translate error correctly', () => {
-    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY')))
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru, data tidak lengkap'));
-    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru, tipe data tidak sesuai'));
-    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.USERNAME_EXCEED_CHAR_LIMIT')))
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_EXCEED_CHAR_LIMIT')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru, username maksimal 50 karakter'));
-    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER_ENTITY.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru, username mengandung karakter terlarang'));
-    expect(DomainErrorTranslator.translate(new Error('USER_LOGIN_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY')))
+    expect(DomainErrorTranslator.translate(new Error('USER_LOGIN.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY')))
       .toStrictEqual(new InvariantError('harus mengirimkan username dan password'));
-    expect(DomainErrorTranslator.translate(new Error('USER_LOGIN_ENTITY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
+    expect(DomainErrorTranslator.translate(new Error('USER_LOGIN.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
       .toStrictEqual(new InvariantError('username dan password harus string'));
-    expect(DomainErrorTranslator.translate(new Error('NEW_THREAD_ENTITY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY')))
+    expect(DomainErrorTranslator.translate(new Error('NEW_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY')))
       .toStrictEqual(new InvariantError('harus mengirimkan judul dan isi thread'));
-    expect(DomainErrorTranslator.translate(new Error('NEW_THREAD_ENTITY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
+    expect(DomainErrorTranslator.translate(new Error('NEW_THREAD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')))
       .toStrictEqual(new InvariantError('judul dan isi thread harus string'));
     expect(DomainErrorTranslator.translate(new Error('PUT_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN')))
       .toStrictEqual(new InvariantError('harus mengirimkan token refresh'));
