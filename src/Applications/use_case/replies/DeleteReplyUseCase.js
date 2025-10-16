71c7d8c2196bf67bfcb41d0517a7a2e17af78d7a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-16 11:51:22 +0700

    Provide payload validation

--- src/Applications/use_case/replies/DeleteReplyUseCase.js
+++ src/Applications/use_case/replies/DeleteReplyUseCase.js
@@ -10,14 +10,32 @@ class DeleteReplyUseCase {
   }
 
   async execute(payload) {
-    const {
-      threadId, commentId, replyId, userId
-    } = payload;
+    this._verifyPayload(payload);
+    const { threadId, commentId, replyId, owner } = payload;
 
     await this._threadRepository.verifyThreadExists(threadId);
     await this._commentRepository.verifyCommentExists(commentId);
-    await this._replyRepository.verifyReplyOwner(replyId, userId);
+    await this._replyRepository.verifyReplyOwner(replyId, owner);
     await this._replyRepository.softDeleteReplyById(replyId);
+  }
+
+  _verifyPayload(payload) {
+    const {
+      threadId, commentId, replyId, owner
+    } = payload;
+
+    if (!threadId || !commentId || !replyId || !owner) {
+      throw new Error('DELETE_REPLY_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof threadId !== 'string'
+      || typeof commentId !== 'string'
+      || typeof replyId !== 'string'
+      || typeof owner !== 'string'
+    ) {
+      throw new Error('DELETE_REPLY_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
   }
 }
 
