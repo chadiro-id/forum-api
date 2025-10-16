b52cc3220079cb5c5db232fa0cd79d5d82daa83a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-16 19:54:41 +0700

    Import and configure replies table helper

--- tests/db_helper/postgres/index.js
+++ tests/db_helper/postgres/index.js
@@ -5,10 +5,12 @@ const authentications = require('./authentications');
 const users = require('./users');
 const threads = require('./threads');
 const comments = require('./comments');
+const replies = require('./replies');
 
 module.exports = {
   usersTable: users(pool),
   authenticationsTable: authentications(pool),
   threadsTable: threads(pool),
   commentsTable: comments(pool),
+  repliesTable: replies(pool),
 };
