5d5581ac8ee1a6c7b2c1b719d17d4674cc81fd65
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-16 22:01:43 +0700

    Describe get detail thread enpoint with deleted comment and reply

--- src/Infrastructures/http/_test/threads.test.js
+++ src/Infrastructures/http/_test/threads.test.js
@@ -5,15 +5,21 @@ const {
   usersTable,
   authenticationsTable,
   threadsTable,
+  commentsTable,
+  repliesTable,
 } = require('../../../../tests/helper/postgres');
 
-let currentUser;
-let currentUserAuth;
+let userA;
+let userB;
+let userAuthA;
+let userAuthB;
 
 beforeAll(async () => {
   await serverTest.init();
-  currentUser = await usersTable.add({ username: 'whoami' });
-  currentUserAuth = await getUserAuth({ username: 'whoami' });
+  userA = await usersTable.add({ id: 'user-123', username: 'whoami' });
+  userAuthA = await getUserAuth({ ...userA });
+  userB = await usersTable.add({ id: 'user-456', username: 'johndoe' });
+  userAuthB = await getUserAuth({ ...userB });
 });
 
 afterAll(async () => {
@@ -23,6 +29,18 @@ afterAll(async () => {
 });
 
 describe('Threads Endpoints', () => {
+  let authorizationUserA;
+  let authorizationUserB;
+
+  beforeAll(async () => {
+    authorizationUserA = {
+      Authorization: `Bearer ${userAuthA.accessToken}`
+    };
+    authorizationUserB = {
+      Authorization: `Bearer ${userAuthB.accessToken}`
+    };
+  });
+
   beforeEach(async () => {
     await serverTest.init();
   });
@@ -32,17 +50,10 @@ describe('Threads Endpoints', () => {
   });
 
   describe('POST /threads', () => {
-    let authorization;
     const dummyPayload = {
       title: 'Judul thread',
       body: 'Sebuah thread',
     };
-
-    beforeAll(async () => {
-      authorization = {
-        Authorization: `Bearer ${currentUserAuth.accessToken}`
-      };
-    });
 
     it('should response 401 when request with no authentication', async () => {
       const options = {
@@ -59,7 +70,7 @@ describe('Threads Endpoints', () => {
 
     it('should response 400 when request payload not contain needed property', async () => {
       const options = {
-        headers: { ...authorization },
+        headers: { ...authorizationUserA },
         payload: {}
       };
 
@@ -74,7 +85,7 @@ describe('Threads Endpoints', () => {
 
     it('should response 400 when request payload does not meet data type specification', async () => {
       const options = {
-        headers: { ...authorization },
+        headers: { ...authorizationUserA },
         payload: { ...dummyPayload, body: 123 },
       };
 
@@ -89,7 +100,7 @@ describe('Threads Endpoints', () => {
 
     it('should response 201 and return the persisted thread', async () => {
       const options = {
-        headers: { ...authorization },
+        headers: { ...authorizationUserA },
         payload: { ...dummyPayload },
       };
 
@@ -102,19 +113,28 @@ describe('Threads Endpoints', () => {
       expect(responseJson.data.addedThread).toEqual({
         id: expect.stringContaining('thread-'),
         title: dummyPayload.title,
-        owner: currentUser.id,
+        owner: userA.id,
       });
     });
   });
 
   describe('GET /threads/{threadId}', () => {
     let threadId;
+    let commentId1;
+    let commentId2;
+    let replyId2;
 
-    beforeEach(async () => {
-      threadId = await threadsTable.add({ owner: currentUser.id });
+    beforeAll(async () => {
+      threadId = await threadsTable.add({ owner: userA.id });
+      commentId1 = await commentsTable.add({ id: 'comment-123', threadId, owner: userA.id });
+      commentId2 = await commentsTable.add({ id: 'comment-456', threadId, owner: userB.id });
+      await repliesTable.add({ id: 'reply-123', commentId: commentId1, owner: userB.id });
+      replyId2 = await repliesTable.add({ id: 'reply-456', commentId: commentId1, owner: userA.id });
     });
 
-    afterEach(async () => {
+    afterAll(async () => {
+      await repliesTable.clean();
+      await commentsTable.clean();
       await threadsTable.clean();
     });
 
@@ -130,9 +150,60 @@ describe('Threads Endpoints', () => {
       expect(responseJson.data.thread.title).toEqual('Sebuah thread');
       expect(responseJson.data.thread.body).toEqual('Isi thread');
       expect(Date.parse(responseJson.data.thread.date)).not.toBeNaN();
-      expect(responseJson.data.thread.username).toBe(currentUser.username);
+      expect(responseJson.data.thread.username).toBe(userA.username);
       expect(responseJson.data.thread.comments).toEqual(expect.any(Array));
-      expect(responseJson.data.thread.comments).toHaveLength(0);
+      expect(responseJson.data.thread.comments).toHaveLength(2);
+
+      const [comment1, comment2] = responseJson.data.thread.comments;
+
+      expect(comment1).toEqual(expect.any(Object));
+      expect(comment1.id).toEqual('comment-123');
+      expect(comment1.username).toEqual(userA.username);
+      expect(comment1.content).toEqual('Sebuah komentar');
+      expect(Date.parse(comment1.date)).not.toBeNaN();
+      expect(comment1.replies).toEqual(expect.any(Array));
+      expect(comment1.replies).toHaveLength(2);
+      expect(comment1.replies).toEqual(
+        expect.arrayOf({
+          id: expect.stringContaining('reply-'),
+          username: expect.any(String),
+          content: 'Sebuah balasan',
+          date: expect.any(String)
+        })
+      );
+
+      expect(comment2).toEqual(expect.any(Object));
+      expect(comment2.id).toEqual('comment-456');
+      expect(comment2.username).toEqual(userB.username);
+      expect(comment2.content).toEqual('Sebuah komentar');
+      expect(Date.parse(comment2.date)).not.toBeNaN();
+      expect(comment2.replies).toEqual(expect.any(Array));
+      expect(comment2.replies).toHaveLength(0);
+    });
+
+    it('should response 200 and return deleted comment and reply correctly', async () => {
+      const comment2endpoint = `/threads/${threadId}/comments/${commentId2}`;
+      const comment2options = {
+        headers: { ...authorizationUserB }
+      };
+      await serverTest.delete(comment2endpoint, comment2options);
+
+      const reply2endpoint = `/threads/${threadId}/comments/${commentId2}/replies/${replyId2}`;
+      const reply2options = {
+        headers: { ...authorizationUserA }
+      };
+      await serverTest.delete(reply2endpoint, reply2options);
+
+      const response = await serverTest.get(`/threads/${threadId}`);
+      expect(response.statusCode).toBe(200);
+
+      const responseJson = JSON.parse(response.payload);
+      const [comment1, comment2] = responseJson.data.thread.comments;
+      expect(comment2.content).toEqual('**komentar telah dihapus**');
+
+      const [reply1, reply2] = comment1.replies;
+      expect(reply1.content).toEqual('Sebuah balasan');
+      expect(reply2.content).toEqual('**balasan telah dihapus**');
     });
   });
 });
