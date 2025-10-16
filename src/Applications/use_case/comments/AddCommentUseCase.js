29f204148e87e0d75c77edb70cff5be5bac33a16
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-16 09:25:20 +0700

    Update business logic, added comment verification

--- src/Applications/use_case/comments/AddCommentUseCase.js
+++ src/Applications/use_case/comments/AddCommentUseCase.js
@@ -11,23 +11,16 @@ class AddCommentUseCase {
   }
 
   async execute(payload) {
-    if (payload instanceof NewComment === false) {
-      throw new Error('ADD_COMMENT_USE_CASE.PAYLOAD_MUST_BE_INSTANCE_OF_NEWCOMMENT');
+    const newComment = new NewComment(payload);
+
+    await this._threadRepository.verifyThreadExists(newComment.threadId);
+    const addedComment = await this._commentRepository.addComment(newComment);
+
+    if (addedComment instanceof AddedComment === false) {
+      throw new Error('ADD_COMMENT_USE_CASE.ADDED_COMMENT_MUST_BE_INSTANCE_OF_ADDED_COMMENT_ENTITY');
     }
-    const { threadId, owner, content } = payload;
 
-    await this._threadRepository.verifyThreadExists(threadId);
-    const addedCommentId = await this._commentRepository.addComment({
-      thread_id: threadId,
-      owner_id: owner,
-      content: content
-    });
-
-    return new AddedComment({
-      id: addedCommentId,
-      content,
-      owner
-    });
+    return addedComment;
   }
 }
 
