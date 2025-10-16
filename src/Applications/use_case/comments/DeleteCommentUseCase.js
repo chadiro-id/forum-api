886389d75c55b94d066ae79974cd5b93587fbd24
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-16 12:57:38 +0700

    Provide payload validation

--- src/Applications/use_case/comments/DeleteCommentUseCase.js
+++ src/Applications/use_case/comments/DeleteCommentUseCase.js
@@ -8,11 +8,28 @@ class DeleteCommentUseCase {
   }
 
   async execute(payload) {
-    const { threadId, commentId, credentialId } = payload;
+    this._verifyPayload(payload);
+    const { threadId, commentId, owner } = payload;
 
     await this._threadRepository.verifyThreadExists(threadId);
-    await this._commentRepository.verifyCommentOwner(commentId, credentialId);
+    await this._commentRepository.verifyCommentOwner(commentId, owner);
     await this._commentRepository.softDeleteCommentById(commentId);
+  }
+
+  _verifyPayload(payload) {
+    const { threadId, commentId, owner } = payload;
+
+    if (!threadId || !commentId || !owner) {
+      throw new Error('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof threadId !== 'string'
+      || typeof commentId !== 'string'
+      || typeof owner !== 'string'
+    ) {
+      throw new Error('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
   }
 }
 
