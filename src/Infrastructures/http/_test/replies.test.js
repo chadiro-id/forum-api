8ceb631779e0a538665a457c4b7e8808a16e03e7
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-16 18:08:15 +0700

    Describe test for post reply endpoint

--- src/Infrastructures/http/_test/replies.test.js
+++ src/Infrastructures/http/_test/replies.test.js
@@ -30,7 +30,7 @@ describe('Replies Endpoints', () => {
 
   beforeAll(async () => {
     threadId = await threadsTable.add({ owner: userA.id });
-    commentId = await commentsTable.add({ owner: userA.id });
+    commentId = await commentsTable.add({ threadId, owner: userA.id });
 
     authorizationUserA = {
       Authorization: `Bearer ${userAuthA.accessToken}`
@@ -63,7 +63,7 @@ describe('Replies Endpoints', () => {
       expect(responseJson.message).toEqual('Missing authentication');
     });
 
-    it('should response 404 when request for a thread that does not exists', async () => {
+    it('should response 404 when thread does not exists', async () => {
       const endpoint = `/threads/xxx/comments/${commentId}/replies`;
       const options = {
         headers: { ...authorizationUserA },
@@ -76,7 +76,74 @@ describe('Replies Endpoints', () => {
       expect(response.statusCode).toBe(404);
       expect(responseJson.status).toBe('fail');
       expect(responseJson.message).toEqual(expect.any(String));
-      expect(responseJson.message).not.toBe('');
+      expect(responseJson.message.trim()).not.toBe('');
+    });
+
+    it('should response 404 when comment does not exists', async () => {
+      const endpoint = `/threads/${threadId}/comments/xxx/replies`;
+      const options = {
+        headers: { ...authorizationUserA },
+        payload: { content: 'Sebuah balasan' },
+      };
+
+      const response = await serverTest.post(endpoint, options);
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toBe(404);
+      expect(responseJson.status).toBe('fail');
+      expect(responseJson.message).toEqual(expect.any(String));
+      expect(responseJson.message.trim()).not.toBe('');
+    });
+
+    it('should response 400 when payload not contain needed property', async () => {
+      const endpoint = `/threads/${threadId}/comments/${commentId}/replies`;
+      const options = {
+        headers: { ...authorizationUserA },
+        payload: { contents: 'Incorrect property name' }
+      };
+
+      const response = await serverTest.post(endpoint, options);
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toBe(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(expect.any(String));
+      expect(responseJson.message.trim()).not.toBe('');
+    });
+
+    it('should response 400 when payload has wrong data type', async () => {
+      const endpoint = `/threads/${threadId}/comments/${commentId}/replies`;
+      const options = {
+        headers: { ...authorizationUserA },
+        payload: { content: 123 }
+      };
+
+      const response = await serverTest.post(endpoint, options);
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toBe(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(expect.any(String));
+      expect(responseJson.message.trim()).not.toBe('');
+    });
+
+    it('should response 201 and the persisted reply', async () => {
+      const endpoint = `/threads/${threadId}/comments/${commentId}/replies`;
+      const options = {
+        headers: { ...authorizationUserA },
+        payload: { content: 'Sebuah balasan' }
+      };
+
+      const response = await serverTest.post(endpoint, options);
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toBe(201);
+      expect(responseJson.status).toBe('success');
+      expect(responseJson.data).toEqual(expect.any(Object));
+      expect(responseJson.data.addedReply).toEqual(expect.any(Object));
+      expect(responseJson.data.addedReply.id).toEqual(expect.stringContaining('reply-'));
+      expect(responseJson.data.addedReply.content).toEqual('Sebuah balasan');
+      expect(responseJson.data.addedReply.owner).toEqual(userA.id);
     });
   });
 });
