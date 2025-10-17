1d2931a63218711ae0af4c0ef8d713ae5ace2bb7
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-17 22:52:22 +0700

    New: comment repository integrations test

---
+++ src/Infrastructures/repository/_test/CommentRepositoryPostgres.integration.test.js
@@ -0,0 +1,58 @@
+const pool = require('../../database/postgres/pool');
+const {
+  usersTable,
+  threadsTable,
+  commentsTable,
+} = require('../../../../tests/helper/postgres');
+const CommentRepositoryPostgres = require('../CommentRepositoryPostgres');
+const NewComment = require('../../../Domains/comments/entities/NewComment');
+const AddedComment = require('../../../Domains/comments/entities/AddedComment');
+
+describe('[Integration] CommentRepositoryPostgres', () => {
+  let commentRepo;
+  let currentUser;
+  let threadId;
+
+  beforeAll(async () => {
+    commentRepo = new CommentRepositoryPostgres(pool, () => '123');
+    currentUser = await usersTable.add({ username: 'johndoe' });
+    threadId = await threadsTable.add({});
+  });
+
+  beforeEach(async () => {
+    await commentsTable.clean();
+  });
+
+  afterAll(async () => {
+    await threadsTable.clean();
+    await usersTable.clean();
+    await pool.end();
+  });
+
+  describe('addComment', () => {
+    let newComment;
+
+    beforeAll(() => {
+      newComment = new NewComment({
+        threadId,
+        content: 'Sebuah komentar',
+        owner: currentUser.id,
+      });
+    });
+
+    it('should persist the NewComment entity', async () => {
+      await commentRepo.addComment(newComment);
+      const comments = await commentsTable.findById('comment-123');
+      expect(comments).toHaveLength(1);
+    });
+
+    it('should return AddedComment entity', async () => {
+      const addedComment = await commentRepo.addComment(newComment);
+      expect(addedComment).toStrictEqual(new AddedComment({
+        id: 'comment-123',
+        content: 'Sebuah komentar',
+        owner: currentUser.id,
+      }));
+    });
+  });
+});
