3fe3dfdf391c45df7257262aa3fac641196694c3
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-17 17:45:39 +0700

    Remove unused config key

--- src/Commons/config.js
+++ src/Commons/config.js
@@ -1,17 +1,9 @@
 /* istanbul ignore file */
 const config = {
-  environment: process.env.NODE_ENV,
   app: {
     host: process.env.HOST,
     port: process.env.PORT,
     debug: ['development'].includes(process.env.NODE_ENV) ? { request: ['error'] } : {},
-  },
-  pgTest: {
-    host: process.env.PGHOST_TEST,
-    port: process.env.PGPORT_TEST,
-    user: process.env.PGUSER_TEST,
-    password: process.env.PGPASSWORD_TEST,
-    database: process.env.PGDATABASE_TEST,
   },
   tokenize: {
     accessTokenKey: process.env.ACCESS_TOKEN_KEY,
