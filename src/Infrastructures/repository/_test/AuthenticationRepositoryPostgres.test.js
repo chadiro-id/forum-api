88574a466de71b5989a94e0593ba6d5017a0c029
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-17 12:29:58 +0700

    Describe add token test scenario

--- src/Infrastructures/repository/_test/AuthenticationRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/AuthenticationRepositoryPostgres.test.js
@@ -6,4 +6,37 @@ describe('[Unit] AuthenticationRepositoryPostgres', () => {
     const repo = new AuthenticationRepositoryPostgres({}, () => '');
     expect(repo).toBeInstanceOf(AuthenticationRepository);
   });
+
+  describe('Methods and Pool Query', () => {
+    let mockPool;
+    let authenticationRepo;
+
+    beforeEach(() => {
+      mockPool = {
+        query: jest.fn(),
+      };
+      authenticationRepo = new AuthenticationRepositoryPostgres(mockPool, () => '');
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    describe('addToken', () => {
+      it('should correctly persist the token', async () => {
+        mockPool.query.mockResolvedValue();
+
+        await expect(authenticationRepo.addToken('token'))
+          .resolves.not.toThrow();
+
+        expect(mockPool.query).toHaveBeenCalledTimes(1);
+        expect(mockPool.query).toHaveBeenCalledWith(
+          expect.objectContaining({
+            text: expect.stringContaining('INSERT INTO authentications'),
+            values: ['token']
+          })
+        );
+      });
+    });
+  });
 });
