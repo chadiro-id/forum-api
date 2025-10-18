67a96d03526affc44363d4f67a8d4cdad29545c0
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-18 17:41:25 +0700

    Describe failure cases scenario

--- src/Applications/use_case/users/_test/AddUserUseCase.test.js
+++ src/Applications/use_case/users/_test/AddUserUseCase.test.js
@@ -5,12 +5,115 @@ const RegisterUser = require('../../../../Domains/users/entities/RegisterUser');
 const RegisteredUser = require('../../../../Domains/users/entities/RegisteredUser');
 
 describe('AddUserUseCase', () => {
-  describe('when executed', () => {
+  const dummyPayload = {
+    username: 'johndoe',
+    password: 'supersecret^_@007',
+    fullname: 'John Doe',
+  };
+
+  let mockUserRepo;
+  let mockPasswordHash;
+
+  beforeEach(() => {
+    mockUserRepo = {
+      addUser: jest.fn(),
+      verifyAvailableUsername: jest.fn(),
+    };
+
+    mockPasswordHash = {
+      hash: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('Failure cases', () => {
+    it('should throw error when payload not provided correctly', async () => {
+      const useCase = new AddUserUseCase({});
+
+      await expect(useCase.execute()).rejects.toThrow();
+      await expect(useCase.execute([])).rejects.toThrow();
+      await expect(useCase.execute({ ...dummyPayload, username: 'who am i' })).rejects.toThrow();
+    });
+
+    it('should propagate error when username already taken', async () => {
+      mockUserRepo.verifyAvailableUsername.mockRejectedValue(new Error());
+
+      const useCase = new AddUserUseCase({
+        userRepository: mockUserRepo,
+        passwordHash: mockPasswordHash,
+      });
+
+      await expect(useCase.execute({ ...dummyPayload }))
+        .rejects.toThrow();
+
+      expect(mockUserRepo.verifyAvailableUsername).toHaveBeenCalledTimes(1);
+      expect(mockUserRepo.verifyAvailableUsername).toHaveBeenCalledWith(dummyPayload.username);
+      expect(mockPasswordHash.hash).not.toHaveBeenCalled();
+    });
+
+    it('should propagate error when addUser fails', async () => {
+      mockUserRepo.verifyAvailableUsername.mockResolvedValue();
+      mockPasswordHash.hash.mockResolvedValue('encrypted_password');
+      mockUserRepo.addUser.mockRejectedValue(new Error());
+
+      const useCase = new AddUserUseCase({
+        userRepository: mockUserRepo,
+        passwordHash: mockPasswordHash,
+      });
+
+      await expect(useCase.execute({ ...dummyPayload }))
+        .rejects.toThrow();
+
+      expect(mockUserRepo.verifyAvailableUsername).toHaveBeenCalledTimes(1);
+      expect(mockUserRepo.verifyAvailableUsername).toHaveBeenCalledWith(dummyPayload.username);
+      expect(mockPasswordHash.hash).toHaveBeenCalledTimes(1);
+      expect(mockUserRepo.addUser).toHaveBeenCalledTimes(1);
+      expect(mockUserRepo.addUser).toHaveBeenCalledWith(new RegisterUser({
+        username: dummyPayload.username,
+        fullname: dummyPayload.fullname,
+        password: 'encrypted_password',
+      }));
+    });
+
+    it('should throw error when the addedUser not an instance of AddedUser entity', async () => {
+      mockUserRepo.verifyAvailableUsername.mockResolvedValue();
+      mockPasswordHash.hash.mockResolvedValue('encrypted_password');
+      mockUserRepo.addUser.mockResolvedValue({
+        id: 'user-123',
+        username: dummyPayload.username,
+        fullname: dummyPayload.fullname,
+      });
+
+      const useCase = new AddUserUseCase({
+        userRepository: mockUserRepo,
+        passwordHash: mockPasswordHash,
+      });
+
+      await expect(useCase.execute({ ...dummyPayload }))
+        .rejects
+        .toThrow('ADD_USER_USE_CASE.ADDED_USER_MUST_BE_INSTANCE_OF_ADDED_USER_ENTITY');
+
+      expect(mockUserRepo.verifyAvailableUsername).toHaveBeenCalledTimes(1);
+      expect(mockUserRepo.verifyAvailableUsername).toHaveBeenCalledWith(dummyPayload.username);
+      expect(mockPasswordHash.hash).toHaveBeenCalledTimes(1);
+      expect(mockUserRepo.addUser).toHaveBeenCalledTimes(1);
+      expect(mockUserRepo.addUser).toHaveBeenCalledWith(new RegisterUser({
+        username: dummyPayload.username,
+        fullname: dummyPayload.fullname,
+        password: 'encrypted_password',
+      }));
+    });
+  });
+
+  describe('Successful execution', () => {
     it('should orchestrating the add user action correctly', async () => {
       const useCasePayload = {
-        username: 'forumapi',
+        username: 'johndoe',
         password: 'super_secret',
-        fullname: 'Forum Api',
+        fullname: 'John Doe',
       };
 
       const mockRegisteredUser = new RegisteredUser({
