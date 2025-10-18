9d227e752ce7e0cbba0c10f9bea1f7c1fedb21aa
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-19 05:31:04 +0700

    Modify mock implementation

--- src/Applications/use_case/authentications/_test/AddAuthenticationUseCase.test.js
+++ src/Applications/use_case/authentications/_test/AddAuthenticationUseCase.test.js
@@ -1,67 +1,67 @@
-const AuthenticationRepository = require('../../../../Domains/authentications/AuthenticationRepository');
 const UserAuthentication = require('../../../../Domains/authentications/entities/UserAuthentication');
-const UserRepository = require('../../../../Domains/users/UserRepository');
-const AuthenticationTokenManager = require('../../../security/AuthenticationTokenManager');
-const PasswordHash = require('../../../security/PasswordHash');
 const AddAuthenticationUseCase = require('../AddAuthenticationUseCase');
 
 describe('AddAuthenticationUseCase', () => {
-  describe('when executed', () => {
+  const dummyPayload = {
+    username: 'johndoe',
+    password: 'secret^_123',
+  };
+
+  let mockUserRepo;
+  let mockAuthRepo;
+  let mockTokenManager;
+  let mockPasswordHash;
+
+  beforeEach(() => {
+    mockUserRepo = {
+      getPasswordByUsername: jest.fn(),
+      getIdByUsername: jest.fn(),
+    };
+    mockAuthRepo = {
+      addToken: jest.fn(),
+    };
+    mockTokenManager = {
+      createAccessToken: jest.fn(),
+      createRefreshToken: jest.fn(),
+    };
+    mockPasswordHash = {
+      comparePassword: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('Successful executions', () => {
     it('should orchestrating the add authentication action correctly', async () => {
-      const useCasePayload = {
-        username: 'forumapi',
-        password: 'secret^_123',
-      };
+      mockUserRepo.getPasswordByUsername.mockResolvedValue('encrypted_password');
+      mockPasswordHash.comparePassword.mockResolvedValue();
+      mockTokenManager.createAccessToken.mockResolvedValue('access_token');
+      mockTokenManager.createRefreshToken.mockResolvedValue('refresh_token');
+      mockUserRepo.getIdByUsername.mockResolvedValue('user-123');
+      mockAuthRepo.addToken.mockResolvedValue();
 
-      const mockedAuthentication = new UserAuthentication({
-        accessToken: 'access_token',
-        refreshToken: 'refresh_token',
-      });
-
-      const mockUserRepository = new UserRepository();
-      const mockAuthenticationRepository = new AuthenticationRepository();
-      const mockAuthenticationTokenManager = new AuthenticationTokenManager();
-      const mockPasswordHash = new PasswordHash();
-
-      mockUserRepository.getPasswordByUsername = jest.fn()
-        .mockImplementation(() => Promise.resolve('encrypted_password'));
-      mockPasswordHash.comparePassword = jest.fn()
-        .mockImplementation(() => Promise.resolve());
-      mockAuthenticationTokenManager.createAccessToken = jest.fn()
-        .mockImplementation(() => Promise.resolve(mockedAuthentication.accessToken));
-      mockAuthenticationTokenManager.createRefreshToken = jest.fn()
-        .mockImplementation(() => Promise.resolve(mockedAuthentication.refreshToken));
-      mockUserRepository.getIdByUsername = jest.fn()
-        .mockImplementation(() => Promise.resolve('user-123'));
-      mockAuthenticationRepository.addToken = jest.fn()
-        .mockImplementation(() => Promise.resolve());
-
-      const addAuthenticationUseCase = new AddAuthenticationUseCase({
-        userRepository: mockUserRepository,
-        authenticationRepository: mockAuthenticationRepository,
-        authenticationTokenManager: mockAuthenticationTokenManager,
+      const useCase = new AddAuthenticationUseCase({
+        userRepository: mockUserRepo,
+        authenticationRepository: mockAuthRepo,
+        authenticationTokenManager: mockTokenManager,
         passwordHash: mockPasswordHash,
       });
 
-      const actualUserAuthentication = await addAuthenticationUseCase.execute(useCasePayload);
+      const actualUserAuthentication = await useCase.execute({ ...dummyPayload });
 
       expect(actualUserAuthentication).toStrictEqual(new UserAuthentication({
         accessToken: 'access_token',
         refreshToken: 'refresh_token',
       }));
 
-      expect(mockUserRepository.getPasswordByUsername)
-        .toHaveBeenCalledWith('forumapi');
-      expect(mockPasswordHash.comparePassword)
-        .toHaveBeenCalledWith('secret^_123', 'encrypted_password');
-      expect(mockUserRepository.getIdByUsername)
-        .toHaveBeenCalledWith('forumapi');
-      expect(mockAuthenticationTokenManager.createAccessToken)
-        .toHaveBeenCalledWith({ username: 'forumapi', id: 'user-123' });
-      expect(mockAuthenticationTokenManager.createRefreshToken)
-        .toHaveBeenCalledWith({ username: 'forumapi', id: 'user-123' });
-      expect(mockAuthenticationRepository.addToken)
-        .toHaveBeenCalledWith(mockedAuthentication.refreshToken);
+      expect(mockUserRepo.getPasswordByUsername).toHaveBeenCalledWith('johndoe');
+      expect(mockPasswordHash.comparePassword).toHaveBeenCalledWith('secret^_123', 'encrypted_password');
+      expect(mockUserRepo.getIdByUsername).toHaveBeenCalledWith('johndoe');
+      expect(mockTokenManager.createAccessToken).toHaveBeenCalledWith({ username: 'johndoe', id: 'user-123' });
+      expect(mockTokenManager.createRefreshToken).toHaveBeenCalledWith({ username: 'johndoe', id: 'user-123' });
+      expect(mockAuthRepo.addToken).toHaveBeenCalledWith('refresh_token');
     });
   });
 });
