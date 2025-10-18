282d6622f1873ee9244968a72e5beeecd464340b
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-18 07:04:27 +0700

    Describe get replies by comment ids

--- src/Infrastructures/repository/_test/ReplyRepositoryPostgres.integration.test.js
+++ src/Infrastructures/repository/_test/ReplyRepositoryPostgres.integration.test.js
@@ -8,18 +8,23 @@ const {
 const ReplyRepositoryPostgres = require('../ReplyRepositoryPostgres');
 const NewReply = require('../../../Domains/replies/entities/NewReply');
 const AddedReply = require('../../../Domains/replies/entities/AddedReply');
+const Reply = require('../../../Domains/replies/entities/Reply');
 
 describe('[Integration] ReplyRepositoryPostgres', () => {
   let replyRepo;
-  let currentUser;
+  let userA;
+  let userB;
   let thread;
-  let comment;
+  let commentA;
+  let commentB;
 
   beforeAll(async () => {
     replyRepo = new ReplyRepositoryPostgres(pool, () => '123');
-    currentUser = await usersTable.add({ username: 'johndoe' });
-    thread = await threadsTable.add({ owner: currentUser.id });
-    comment = await commentsTable.add({ threadId: thread.id, owner: currentUser.id });
+    userA = await usersTable.add({ id: 'user-123', username: 'johndoe' });
+    userB = await usersTable.add({ id: 'user-456', username: 'whoami' });
+    thread = await threadsTable.add({ owner: userA.id });
+    commentA = await commentsTable.add({ id: 'comment-123', threadId: thread.id, owner: userA.id });
+    commentB = await commentsTable.add({ id: 'comment-456', threadId: thread.id, owner: userB.id });
   });
 
   beforeEach(async () => {
@@ -39,24 +44,74 @@ describe('[Integration] ReplyRepositoryPostgres', () => {
 
     beforeAll(() => {
       newReply = new NewReply({
-        commentId: comment.id,
+        commentId: commentA.id,
         content: 'Sebuah balasan',
-        owner: currentUser.id,
+        owner: userA.id,
       });
     });
 
     it('should persist the NewReply entity correctly', async () => {
       await replyRepo.addReply(newReply);
+
       const replies = await repliesTable.findById('reply-123');
       expect(replies).toHaveLength(1);
     });
 
     it('should return the AddedReply entity correctly', async () => {
       const addedReply = await replyRepo.addReply(newReply);
+
       expect(addedReply).toStrictEqual(new AddedReply({
         id: 'reply-123',
         content: 'Sebuah balasan',
         owner: 'user-123',
+      }));
+    });
+  });
+
+  describe('getRepliesByCommentIds', () => {
+    it('should return an empty array when no reply found for related comment ids', async () => {
+      const replies = await replyRepo.getRepliesByCommentIds([commentA.id, commentB.id]);
+
+      expect(replies).toEqual([]);
+    });
+
+    it('should correctly return replies related to comment ids', async () => {
+      const replyA = await repliesTable.add({
+        id: 'reply-123', commentId: commentB.id, owner: userA.id
+      });
+      const replyB = await repliesTable.add({
+        id: 'reply-456', commentId: commentA.id, owner: userB.id
+      });
+      const replyC = await repliesTable.add({
+        id: 'reply-789', commentId: commentB.id, owner: userB.id
+      });
+
+      const replies = await replyRepo.getRepliesByCommentIds([commentA.id, commentB.id]);
+
+      expect(replies).toHaveLength(3);
+      expect(replies[0]).toStrictEqual(new Reply({
+        id: replyA.id,
+        commentId: commentB.id,
+        content: 'Sebuah balasan',
+        username: userA.username,
+        date: replyA.created_at,
+        isDelete: false,
+      }));
+      expect(replies[1]).toStrictEqual(new Reply({
+        id: replyB.id,
+        commentId: commentA.id,
+        content: 'Sebuah balasan',
+        username: userB.username,
+        date: replyB.created_at,
+        isDelete: false,
+      }));
+      expect(replies[2]).toStrictEqual(new Reply({
+        id: replyC.id,
+        commentId: commentB.id,
+        content: 'Sebuah balasan',
+        username: userB.username,
+        date: replyC.created_at,
+        isDelete: false,
       }));
     });
   });
