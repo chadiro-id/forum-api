37c3d7472fa46ca151d9f6833e52f96bde543398
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-18 13:51:22 +0700

    Cleanup comments

--- src/Infrastructures/http/server.js
+++ src/Infrastructures/http/server.js
@@ -2,7 +2,6 @@ const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const config = require('../../Commons/config');
 const ClientError = require('../../Commons/exceptions/ClientError');
-// const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
 
 const users = require('../../Interfaces/http/api/users');
 const authentications = require('../../Interfaces/http/api/authentications');
@@ -87,22 +86,6 @@ const createServer = async (container) => {
     const { response } = request;
 
     if (response instanceof Error) {
-      // const translatedError = DomainErrorTranslator.translate(response);
-
-      // if (translatedError instanceof ClientError) {
-      //   const newResponse = h.response({
-      //     status: 'fail',
-      //     message: translatedError.message,
-      //   });
-
-      //   newResponse.code(translatedError.statusCode);
-      //   return newResponse;
-      // }
-
-      // if (!translatedError.isServer) {
-      //   return h.continue;
-      // }
-
       if (response instanceof ClientError) {
         const newResponse = h.response({
           status: 'fail',
@@ -116,8 +99,6 @@ const createServer = async (container) => {
       if (!response.isServer) {
         return h.continue;
       }
-
-      // console.error('[server] on pre response -> error:', translatedError.message);
 
       const newResponse = h.response({
         status: 'error',
