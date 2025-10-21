a89bdc9e040b91fcd1afd5e20697052878dddf75
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-22 04:41:00 +0700

    Provide payload validation

--- src/Domains/replies/entities/DeleteReply.js
+++ src/Domains/replies/entities/DeleteReply.js
@@ -1,5 +1,24 @@
 class DeleteReply {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { threadId, commentId, replyId, owner } = payload;
+
+    if (!threadId || !commentId || !replyId || !owner) {
+      throw new Error('DELETE_REPLY.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof threadId !== 'string'
+      || typeof commentId !== 'string'
+      || typeof replyId !== 'string'
+      || typeof owner !== 'string'
+    ) {
+      throw new Error('DELETE_REPLY.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
 }
 
 module.exports = DeleteReply;
