e6ec9c403d51ac9b9ca1151b1e0a387927da323f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-21 08:55:50 +0700

    Validator indexer

---
+++ src/Infrastructures/validator/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  users: require('./users'),
+  authentications: require('./authentications'),
+  threads: require('./threads'),
+  comments: require('./comments'),
+  replies: require('./replies')
+};
