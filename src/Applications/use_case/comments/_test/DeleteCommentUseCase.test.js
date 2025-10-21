71d9a59f96044d7a66c36355623588cb56abcbdc
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-22 05:31:43 +0700

    Update incorrect payload scenario

--- src/Applications/use_case/comments/_test/DeleteCommentUseCase.test.js
+++ src/Applications/use_case/comments/_test/DeleteCommentUseCase.test.js
@@ -30,56 +30,9 @@ describe('DeleteCommentUseCase', () => {
   });
 
   describe('Failure cases', () => {
-    it('should throw error when payload not contain needed property', async () => {
-      const missingThreadId = { ...dummyPayload };
-      delete missingThreadId.threadId;
-      const missingCommentId = { ...dummyPayload };
-      delete missingCommentId.commentId;
-      const missingOwner = { ...dummyPayload };
-      delete missingOwner.owner;
-
-      const emptyThreadId = { ...dummyPayload, threadId: '' };
-      const emptyCommentId = { ...dummyPayload, commentId: '' };
-      const emptyOwner = { ...dummyPayload, owner: '' };
-
-      const useCase = new DeleteCommentUseCase({});
-
-      await expect(useCase.execute(missingThreadId))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-      await expect(useCase.execute(missingCommentId))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-      await expect(useCase.execute(missingOwner))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-      await expect(useCase.execute(emptyThreadId))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-      await expect(useCase.execute(emptyCommentId))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-      await expect(useCase.execute(emptyOwner))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-    });
-
-    it('should throw error when payload property does not meet data type specification', async () => {
-      const threadIdNotString = { ...dummyPayload, threadId: 123 };
-      const commentIdNotString = { ...dummyPayload, commentId: [1, 2, 3] };
-      const ownerNotString = { ...dummyPayload, owner: true };
-
-      const useCase = new DeleteCommentUseCase({});
-
-      await expect(useCase.execute(threadIdNotString))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
-      await expect(useCase.execute(commentIdNotString))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
-      await expect(useCase.execute(ownerNotString))
-        .rejects
-        .toThrow('DELETE_COMMENT_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    it('should throw error when payload not provided correctly', async () => {
+      await expect(deleteCommentUseCase.execute()).rejects.toThrow();
+      await expect(deleteCommentUseCase.execute({})).rejects.toThrow();
     });
 
     it('should propagate error when thread not exists', async () => {
