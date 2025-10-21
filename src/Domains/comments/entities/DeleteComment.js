51968769516079c76e3009df0611e4e0e5967414
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-22 05:14:55 +0700

    Provide payload validation

--- src/Domains/comments/entities/DeleteComment.js
+++ src/Domains/comments/entities/DeleteComment.js
@@ -1,5 +1,23 @@
 class DeleteComent {
-  constructor() {}
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload(payload) {
+    const { threadId, commentId, owner } = payload;
+
+    if (!threadId || !commentId || !owner) {
+      throw new Error('DELETE_COMMENT.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof threadId !== 'string'
+      || typeof commentId !== 'string'
+      || typeof owner !== 'string'
+    ) {
+      throw new Error('DELETE_COMMENT.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
 }
 
 module.exports = DeleteComent;
