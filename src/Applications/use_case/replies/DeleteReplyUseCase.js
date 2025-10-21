1886ab196b94e6259d755db660b8cf08cae0eba7
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-22 04:52:28 +0700

    Replace payload verification with delete entity

--- src/Applications/use_case/replies/DeleteReplyUseCase.js
+++ src/Applications/use_case/replies/DeleteReplyUseCase.js
@@ -1,3 +1,5 @@
+const DeleteReply = require('../../../Domains/replies/entities/DeleteReply');
+
 class DeleteReplyUseCase {
   constructor({
     threadRepository,
@@ -10,33 +12,12 @@ class DeleteReplyUseCase {
   }
 
   async execute(payload) {
-    this._verifyPayload(payload);
-
-    const { threadId, commentId, replyId, owner } = payload;
+    const { threadId, commentId, replyId, owner } = new DeleteReply(payload);
 
     await this._threadRepository.verifyThreadExists(threadId);
     await this._commentRepository.verifyCommentExists(commentId);
     await this._replyRepository.verifyReplyOwner(replyId, owner);
     await this._replyRepository.softDeleteReplyById(replyId);
-  }
-
-  _verifyPayload(payload) {
-    const {
-      threadId, commentId, replyId, owner
-    } = payload;
-
-    if (!threadId || !commentId || !replyId || !owner) {
-      throw new Error('DELETE_REPLY_USE_CASE.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-    }
-
-    if (
-      typeof threadId !== 'string'
-      || typeof commentId !== 'string'
-      || typeof replyId !== 'string'
-      || typeof owner !== 'string'
-    ) {
-      throw new Error('DELETE_REPLY_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
-    }
   }
 }
 
