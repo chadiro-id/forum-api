65d612ab53edb09ae68a33d80a1620780a9acbc6
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-25 00:14:29 +0700

    Add assertion http response error

--- tests/helper/assertionsHelper.js
+++ tests/helper/assertionsHelper.js
@@ -11,3 +11,19 @@ exports.assertQueryCalled = (
     })
   );
 };
+
+exports.assertHttpResponseError = (
+  response, statusCode, json = {}
+) => {
+  const resJson = JSON.parse(response.payload);
+  expect(response.statusCode).toBe(statusCode);
+  expect(resJson.status).toEqual(json.status || 'fail');
+  if (json.message) {
+    expect(resJson.message).toEqual(
+      expect.stringContaining(json.message)
+    );
+  } else {
+    expect(resJson.message).toEqual(expect.any(String));
+    expect(resJson.message.trim()).not.toBe('');
+  }
+};
