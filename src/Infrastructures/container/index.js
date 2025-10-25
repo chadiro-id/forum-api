b7481a642f46c15fe4a1d58d52b5a6395c4b7f54
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-25 14:34:24 +0700

    Configure instance options

--- src/Infrastructures/container/index.js
+++ src/Infrastructures/container/index.js
@@ -1,11 +1,23 @@
 const { createContainer } = require('instances-container');
 
 const defaultOption = require('./option');
+const usersOption = require('./users/option');
+const authenticationsOption = require('./authentications/option');
+const threadsOption = require('./threads/option');
+const commentsOption = require('./comments/option');
+const repliesOption = require('./replies/option');
 
 const container = () => {
-  return createContainer([
-    ...defaultOption,
-  ]);
+  const options = [
+    defaultOption,
+    usersOption,
+    authenticationsOption,
+    threadsOption,
+    commentsOption,
+    repliesOption,
+  ];
+
+  return createContainer(options.flat());
 };
 
 module.exports = container;
