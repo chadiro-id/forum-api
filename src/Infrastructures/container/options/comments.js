356fb496394c10272deb325d83d41ba8b204faa7
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-25 17:25:26 +0700

    Manage container options

---
+++ src/Infrastructures/container/options/comments.js
@@ -0,0 +1,60 @@
+const { nanoid } = require('nanoid');
+const pool = require('../../database/postgres/pool');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const CommentRepositoryPostgres = require('../../repository/CommentRepositoryPostgres');
+const AddCommentUseCase = require('../../../Applications/use_case/comments/AddCommentUseCase');
+const DeleteCommentUseCase = require('../../../Applications/use_case/comments/DeleteCommentUseCase');
+
+const options = [
+  {
+    key: CommentRepository.name,
+    Class: CommentRepositoryPostgres,
+    parameter: {
+      dependencies: [
+        {
+          concrete: pool,
+        },
+        {
+          concrete: nanoid,
+        },
+      ],
+    },
+  },
+  {
+    key: AddCommentUseCase.name,
+    Class: AddCommentUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'threadRepository',
+          internal: ThreadRepository.name,
+        },
+        {
+          name: 'commentRepository',
+          internal: CommentRepository.name,
+        },
+      ],
+    },
+  },
+  {
+    key: DeleteCommentUseCase.name,
+    Class: DeleteCommentUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'threadRepository',
+          internal: ThreadRepository.name,
+        },
+        {
+          name: 'commentRepository',
+          internal: CommentRepository.name,
+        },
+      ],
+    },
+  },
+];
+
+module.exports = options;
