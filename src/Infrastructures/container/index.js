795de5181d54e0ba2f501d37586a512ca5f82f1a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-25 17:26:22 +0700

    Update container options

--- src/Infrastructures/container/index.js
+++ src/Infrastructures/container/index.js
@@ -1,11 +1,11 @@
 const { createContainer } = require('instances-container');
 
-const defaultOption = require('./option');
-const usersOption = require('./users/option');
-const authenticationsOption = require('./authentications/option');
-const threadsOption = require('./threads/option');
-const commentsOption = require('./comments/option');
-const repliesOption = require('./replies/option');
+const defaultOption = require('./options/default');
+const usersOption = require('./options/users');
+const authenticationsOption = require('./options/authentications');
+const threadsOption = require('./options/threads');
+const commentsOption = require('./options/comments');
+const repliesOption = require('./options/replies');
 
 const setupContainer = () => {
   const options = [
