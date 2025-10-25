effa2d7bf6214dcd3ace21da5486cf439acc3fb7
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-25 14:05:18 +0700

    Default container options

---
+++ src/Infrastructures/container/option.js
@@ -0,0 +1,41 @@
+const config = require('../../Commons/config');
+const bcrypt = require('bcrypt');
+const Jwt = require('@hapi/jwt');
+
+const PasswordHash = require('../../Applications/security/PasswordHash');
+const BcryptPasswordHash = require('../security/BcryptPasswordHash');
+const AuthenticationTokenManager = require('../../Applications/security/AuthenticationTokenManager');
+const JwtTokenManager = require('../security/JwtTokenManager');
+
+const options = [
+  {
+    key: PasswordHash.name,
+    Class: BcryptPasswordHash,
+    parameter: {
+      dependencies: [
+        {
+          concrete: bcrypt,
+        },
+      ],
+    },
+  },
+  {
+    key: AuthenticationTokenManager.name,
+    Class: JwtTokenManager,
+    parameter: {
+      dependencies: [
+        {
+          concrete: Jwt.token,
+        },
+        {
+          concrete: config.tokenize.accessTokenKey,
+        },
+        {
+          concrete: config.tokenize.refreshTokenKey,
+        },
+      ],
+    },
+  },
+];
+
+module.exports = options;
