bad484b15e53d8f8f0ed9f8f6eec8cbeac610d84
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-27 12:34:30 +0700

    Update configs

--- jest.config.js
+++ jest.config.js
@@ -4,16 +4,11 @@ const config = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/tests/',
-  ],
   testMatch: [
     '**/?(*.)+(test).[jt]s?(x)',
   ],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '<rootDir>/tests/',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
