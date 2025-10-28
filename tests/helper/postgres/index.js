92adecd4695d8431ceeaee6abdd560248e26b346
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-28 22:03:55 +0700

    Simplify postgres test helper

--- tests/helper/postgres/index.js
+++ tests/helper/postgres/index.js
@@ -7,20 +7,10 @@ const threadsTableHelper = require('./threadsTableHelper');
 const commentsTableHelper = require('./commentsTableHelper');
 const repliesTableHelper = require('./repliesTableHelper');
 
-exports.getPool = () => pool;
-
-exports.users = () => usersTableHelper(pool);
-exports.authentications = () => authenticationsTableHelper(pool);
-exports.threads = () => threadsTableHelper(pool);
-exports.comments = () => commentsTableHelper(pool);
-exports.replies = () => repliesTableHelper(pool);
-
-exports.truncate = async () => {
-  const queryText = 'TRUNCATE TABLE users, authentications, threads, comments, replies';
-  await pool.query(queryText);
-};
-
-exports.end = async () => {
-  this.truncate();
-  await pool.end();
+module.exports = {
+  usersTable: usersTableHelper(pool),
+  authenticationsTable: authenticationsTableHelper(pool),
+  threadsTable: threadsTableHelper(pool),
+  commentsTable: commentsTableHelper(pool),
+  repliesTable: repliesTableHelper(pool),
 };
