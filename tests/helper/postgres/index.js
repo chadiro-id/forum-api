bceccf6b7dcdf9dc17f64c628162e8a547870e77
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-28 09:51:54 +0700

    Update db pool

--- tests/helper/postgres/index.js
+++ tests/helper/postgres/index.js
@@ -1,4 +1,5 @@
 /* istanbul ignore file */
+const pool = require('../../../src/Infrastructures/database/postgres/pool');
 
 const authenticationsTableHelper = require('./authenticationsTableHelper');
 const usersTableHelper = require('./usersTableHelper');
@@ -6,34 +7,20 @@ const threadsTableHelper = require('./threadsTableHelper');
 const commentsTableHelper = require('./commentsTableHelper');
 const repliesTableHelper = require('./repliesTableHelper');
 
-let _pool;
-let _users;
-let _authentications;
-let _threads;
-let _comments;
-let _replies;
+exports.getPool = () => pool;
 
-exports.init = (pool) => {
-  _pool = pool;
-  _users = usersTableHelper(pool);
-  _authentications = authenticationsTableHelper(pool);
-  _threads = threadsTableHelper(pool);
-  _comments = commentsTableHelper(pool);
-  _replies = repliesTableHelper(pool);
-};
-
-exports.users = () => _users;
-exports.authentications = () => _authentications;
-exports.threads = () => _threads;
-exports.comments = () => _comments;
-exports.replies = () => _replies;
+exports.users = () => usersTableHelper(pool);
+exports.authentications = () => authenticationsTableHelper(pool);
+exports.threads = () => threadsTableHelper(pool);
+exports.comments = () => commentsTableHelper(pool);
+exports.replies = () => repliesTableHelper(pool);
 
 exports.truncate = async () => {
   const queryText = 'TRUNCATE TABLE users, authentications, threads, comments, replies';
-  await _pool.query(queryText);
+  await pool.query(queryText);
 };
 
 exports.end = async () => {
   this.truncate();
-  await _pool.end();
+  await pool.end();
 };
