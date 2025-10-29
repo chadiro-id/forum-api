255f6b6394df3595b28dbad48755c42bba8fa345
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-29 17:05:10 +0700

    Add helper for comment and reply repositoey assertions

--- tests/helper/assertionsHelper.js
+++ tests/helper/assertionsHelper.js
@@ -1,4 +1,6 @@
 /* istanbul ignore file */
+const Comment = require('../../src/Domains/comments/entities/Comment');
+const Reply = require('../../src/Domains/replies/entities/Reply');
 
 const assertQueryCalled = (
   query, queryTextPart, queryValues
@@ -56,9 +58,35 @@ const expectReplyFromResponse = (reply, expectedSource) => {
   expect(Date.parse(reply.date)).not.toBeNaN();
 };
 
+const expectCommentFromRepository = (comment, expectedSource) => {
+  const expectedContent = expectedSource.is_delete
+    ? '**komentar telah dihapus**'
+    : expectedSource.content;
+
+  expect(comment).toBeInstanceOf(Comment);
+  expect(comment.id).toEqual(expectedSource.id);
+  expect(comment.content).toEqual(expectedContent);
+  expect(comment.username).toEqual(expectedSource.username);
+  expect(comment.date).toEqual(expectedSource.created_at);
+};
+
+const expectReplyFromRepository = (reply, expectedSource) => {
+  const expectedContent = expectedSource.is_delete
+    ? '**balasan telah dihapus**'
+    : expectedSource.content;
+
+  expect(reply).toBeInstanceOf(Reply);
+  expect(reply.id).toEqual(expectedSource.id);
+  expect(reply.content).toEqual(expectedContent);
+  expect(reply.username).toEqual(expectedSource.username);
+  expect(reply.date).toEqual(expectedSource.created_at);
+};
+
 module.exports = {
   assertQueryCalled,
   assertHttpResponseError,
   expectCommentFromResponse,
   expectReplyFromResponse,
+  expectCommentFromRepository,
+  expectReplyFromRepository,
 };
