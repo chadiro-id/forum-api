64f7c71068fd36321a4a8bc9ae8bcc0320bf98bb
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-29 12:45:55 +0700

    Update pool instantiation

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -1,6 +1,17 @@
 /* istanbul ignore file */
 const { Pool } = require('pg');
 
-const pool = new Pool();
+let pool;
+if (process.env.NODE_ENV === 'test') {
+  pool = new Pool({
+    host: process.env.PGHOST_TEST || process.env.PGHOST,
+    database: process.env.PGDATABASE_TEST || process.env.PGDATABASE,
+    user: process.env.PGUSER_TEST || process.env.PGUSER,
+    password: process.env.PGPASSWORD_TEST || process.env.PGPASSWORD,
+    port: process.env.PGPORT_TEST || process.env.PGPORT,
+  });
+} else {
+  pool = new Pool();
+}
 
 module.exports = pool;
