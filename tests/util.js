974ab9e1b75fd1450838ffd419a4255f5d830bc5
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-29 17:52:24 +0700

    Add comment and reply raw data creator

--- tests/util.js
+++ tests/util.js
@@ -7,9 +7,39 @@ const createRawThread = (overrides = {}) => {
     username: 'johndoe',
     created_at: date,
   };
+
   return { ...defaultThread, ...overrides };
+};
+
+const createRawComment = (overrides = {}) => {
+  const date = new Date(Date.now());
+  const defaultComment = {
+    id: 'comment-001',
+    content: 'Sebuah komentar',
+    username: 'johndoe',
+    created_at: date,
+    is_delete: false,
+  };
+
+  return { ...defaultComment, ...overrides };
+};
+
+const createRawReply = (overrides = {}) => {
+  const date = new Date(Date.now());
+  const defaultReply = {
+    id: 'reply-001',
+    comment_id: 'comment-001',
+    content: 'Sebuah balasan',
+    username: 'johndoe',
+    created_at: date,
+    is_delete: false,
+  };
+
+  return { ...defaultReply, ...overrides };
 };
 
 module.exports = {
   createRawThread,
+  createRawComment,
+  createRawReply,
 };
