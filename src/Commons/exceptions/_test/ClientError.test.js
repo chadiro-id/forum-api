7ad46ae5526be2cc73c4f5d1bf76fa8973a21cd3
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-01 14:01:52 +0700

    Update test description to be clear

--- src/Commons/exceptions/_test/ClientError.test.js
+++ src/Commons/exceptions/_test/ClientError.test.js
@@ -1,7 +1,7 @@
 const ClientError = require('../ClientError');
 
 describe('ClientError', () => {
-  it('should throw error when directly use it', () => {
+  it('should throw error when directly instantiated', () => {
     expect(() => new ClientError('')).toThrow('cannot instantiate abstract class');
   });
 });
