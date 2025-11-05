509afe120abc5045c2198080acce00405e88145f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-05 22:57:52 +0700

    Configure translate method

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -1,3 +1,9 @@
-const DomainErrorTranslator = {};
+const directories = Object.assign({}, {});
+
+class DomainErrorTranslator {
+  static translate(error) {
+    return directories[error.message] || error;
+  }
+}
 
 module.exports = DomainErrorTranslator;
