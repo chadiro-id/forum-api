7ead8e061400c064d04f4d9627fce8581f89a3d1
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-06 02:02:07 +0700

    Translate error username not available

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -1,4 +1,21 @@
-const directories = Object.assign({}, {});
+const InvariantError = require('./InvariantError');
+
+const usersUseCaseError = {
+  'ADD_USER_USE_CASE.USERNAME_NOT_AVAILABLE': new InvariantError('username tidak tersedia'),
+};
+const authenticationsUseCaseError = {};
+const threadsUseCaseError = {};
+const commentsUseCaseError = {};
+const repliesUseCaseError = {};
+
+const directories = Object.assign(
+  {},
+  usersUseCaseError,
+  authenticationsUseCaseError,
+  threadsUseCaseError,
+  commentsUseCaseError,
+  repliesUseCaseError
+);
 
 class DomainErrorTranslator {
   static translate(error) {
