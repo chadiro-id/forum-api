c70b89c48c049601e9a569045793d1488bf2227b
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-06 12:05:41 +0700

    Add authentications use case error to translate

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -1,9 +1,12 @@
 const InvariantError = require('./InvariantError');
+const AuthenticationError = require('./AuthenticationError');
 
 const usersUseCaseError = {
   'ADD_USER_USE_CASE.USERNAME_NOT_AVAILABLE': new InvariantError('username tidak tersedia'),
 };
 const authenticationsUseCaseError = {
+  'ADD_AUTHENTICATION_USE_CASE.USER_NOT_EXIST': new InvariantError('user tidak ada, username tidak ditemukan'),
+  'ADD_AUTHENTICATION_USE_CASE.PASSWORD_NOT_MATCH': new AuthenticationError('kredensial yang Anda masukkan salah'),
   'PUT_AUTHENTICATION_USE_CASE.REFRESH_TOKEN_NOT_FOUND': new InvariantError('refresh token tidak ditemukan di database'),
   'DELETE_AUTHENTICATION_USE_CASE.REFRESH_TOKEN_NOT_FOUND': new InvariantError('refresh token tidak ditemukan di database'),
 };
