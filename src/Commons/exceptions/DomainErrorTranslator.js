1a24c93100ee2f1c569b82f3d64fad0bcca8ca7c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-06 08:25:02 +0700

    Translate refresh token not found error

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -3,7 +3,10 @@ const InvariantError = require('./InvariantError');
 const usersUseCaseError = {
   'ADD_USER_USE_CASE.USERNAME_NOT_AVAILABLE': new InvariantError('username tidak tersedia'),
 };
-const authenticationsUseCaseError = {};
+const authenticationsUseCaseError = {
+  'PUT_AUTHENTICATION_USE_CASE.REFRESH_TOKEN_NOT_FOUND': new InvariantError('refresh token tidak ditemukan di database'),
+  'DELETE_AUTHENTICATION_USE_CASE.REFRESH_TOKEN_NOT_FOUND': new InvariantError('refresh token tidak ditemukan di database'),
+};
 const threadsUseCaseError = {};
 const commentsUseCaseError = {};
 const repliesUseCaseError = {};
