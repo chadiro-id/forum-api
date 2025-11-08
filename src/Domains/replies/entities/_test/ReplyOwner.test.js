ec2cb6af7b079f26943270f43e391f62a9f3163f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 14:28:19 +0700

    Add missing needed property test case

--- src/Domains/replies/entities/_test/ReplyOwner.test.js
+++ src/Domains/replies/entities/_test/ReplyOwner.test.js
@@ -7,4 +7,14 @@ describe('ReplyOwner Entity', () => {
     const { owner } = new ReplyOwner(payload);
     expect(owner).toBe(payload.owner);
   });
+
+  it('should throw error when payload not contain needed property', () => {
+    const missingOwner = {};
+    const emptyOwner = { owner: '' };
+
+    expect(() => new ReplyOwner(missingOwner))
+      .toThrow('REPLY_OWNER.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new ReplyOwner(emptyOwner))
+      .toThrow('REPLY_OWNER.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+  });
 });
