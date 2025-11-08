c80f25960d21ab9d240fb29fd86f4ac7984ffe4b
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-09 01:46:12 +0700

    New: Array group utilities

---
+++ src/Applications/utilities/ArrayGroupUtils.js
@@ -0,0 +1,17 @@
+const ArrayGroupUtils = {
+  /**
+   * Mengubah array objek menjadi object yang dikelompokkan berdasarkan kunci.
+   * @param {Array<Object>} array Array yang ingin dikelompokan
+   * @param {string} key Properti yang digunakan untuk pengelompokan
+   * @returns {Object} Objek yang sudah dikelompokan
+   */
+  groupToObjectBy(array = [], key) {
+    return array.reduce((acc, item) => {
+      const groupKey = item[key];
+      (acc[groupKey] ||= []).push(item);
+      return acc;
+    }, {});
+  }
+};
+
+module.exports = ArrayGroupUtils;
