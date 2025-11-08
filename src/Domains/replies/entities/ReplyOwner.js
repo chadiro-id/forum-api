fad87488a28fd0858ca716131a68ffa41004d1eb
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 14:28:54 +0700

    Provide payload verifications

--- src/Domains/replies/entities/ReplyOwner.js
+++ src/Domains/replies/entities/ReplyOwner.js
@@ -1,6 +1,14 @@
 class ReplyOwner {
   constructor(payload) {
+    this._verifyPayload(payload);
+
     this.owner = payload.owner;
+  }
+
+  _verifyPayload({ owner }) {
+    if (!owner) {
+      throw new Error('REPLY_OWNER.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
   }
 }
 
