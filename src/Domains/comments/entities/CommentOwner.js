ef6bf80a4f3944e214bbda68e67924d78b981da8
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 16:00:51 +0700

    Defining needed property and payload verification

--- src/Domains/comments/entities/CommentOwner.js
+++ src/Domains/comments/entities/CommentOwner.js
@@ -1,3 +1,19 @@
-class CommentOwner {}
+class CommentOwner {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    this.owner = payload.owner;
+  }
+
+  _verifyPayload({ owner }) {
+    if (!owner) {
+      throw new Error('COMMENT_OWNER.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof owner !== 'string') {
+      throw new Error('COMMENT_OWNER.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
 
 module.exports = CommentOwner;
