06dcb845827aa6f24d07a6447b5ef013ed7dd790
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 23:57:42 +0700

    Remove empty line

--- src/Applications/use_case/comments/AddCommentUseCase.js
+++ src/Applications/use_case/comments/AddCommentUseCase.js
@@ -19,7 +19,6 @@ class AddCommentUseCase {
     }
 
     const addedComment = await this._commentRepository.addComment(newComment);
-
     if (addedComment instanceof AddedComment === false) {
       throw new Error('ADD_COMMENT_USE_CASE.ADDED_COMMENT_MUST_BE_INSTANCE_OF_ADDED_COMMENT_ENTITY');
     }
