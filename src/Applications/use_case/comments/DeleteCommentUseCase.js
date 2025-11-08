16946602efe346dc32dda3cc4f1352d3f8fc34bf
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 17:21:42 +0700

    Update execution flow

--- src/Applications/use_case/comments/DeleteCommentUseCase.js
+++ src/Applications/use_case/comments/DeleteCommentUseCase.js
@@ -9,23 +9,14 @@ class DeleteCommentUseCase {
     const { threadId, commentId, owner } = new DeleteComment(payload);
 
     const comment = await this._commentRepository.getCommentForDeletion(commentId, threadId);
-    this._verifyCommentDeletion(comment);
-
+    if (comment === null) {
+      throw new Error('DELETE_COMMENT_USE_CASE.COMMENT_NOT_EXIST');
+    }
     if (comment.owner !== owner) {
       throw new Error('DELETE_COMMENT_USE_CASE.OWNER_NOT_MATCH');
     }
 
     await this._commentRepository.softDeleteCommentById(commentId);
-  }
-
-  _verifyCommentDeletion(comment) {
-    if (comment === null) {
-      throw new Error('DELETE_COMMENT_USE_CASE.COMMENT_NOT_EXIST');
-    }
-
-    if (!comment.owner || typeof comment.owner !== 'string') {
-      throw new Error('DELETE_COMMENT_USE_CASE.COMMENT_OWNER_MUST_NON_EMPTY_STRING');
-    }
   }
 }
 
