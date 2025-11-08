9d5092464fd4c23c7a676f351af47f2491ef50f3
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 08:44:25 +0700

    Update assertions

--- src/Commons/utils/_test/DetailThreadMapper.test.js
+++ src/Commons/utils/_test/DetailThreadMapper.test.js
@@ -3,22 +3,23 @@ const DetailThreadMapper = require('../DetailThreadMapper');
 
 describe('DetailThreadMapper', () => {
   it('should correctly map joined database row to DetailThread entity', () => {
+    const timestamp = new Date();
     const dbRow = {
       id: 'thread-123',
       title: 'Detail Thread',
       body: 'Isi detail',
       username: 'john_doe',
-      created_at: new Date('2024-11-01T10:00:00.000Z'),
+      created_at: timestamp,
     };
 
     const detailThread = DetailThreadMapper.toEntity(dbRow);
 
     expect(detailThread).toStrictEqual(new DetailThread({
-      id: 'thread-123',
-      title: 'Detail Thread',
-      body: 'Isi detail',
-      username: 'john_doe',
-      date: new Date('2024-11-01T10:00:00.000Z'),
+      id: dbRow.id,
+      title: dbRow.title,
+      body: dbRow.body,
+      username: dbRow.username,
+      date: dbRow.created_at,
     }));
   });
 });
