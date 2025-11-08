b3c42f63e9754be0df5a5e0b74e26a9e1e0dda87
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 16:19:21 +0700

    Describe map comment list test case

--- src/Commons/utils/_test/CommentMapper.test.js
+++ src/Commons/utils/_test/CommentMapper.test.js
@@ -1,24 +1,32 @@
 const Comment = require('../../../Domains/comments/entities/Comment');
 const CommentMapper = require('../CommentMapper');
+const { createRawComment } = require('../../../../tests/util');
 
 describe('CommentMapper', () => {
-  it('should correctly map database row to Comment entity', () => {
-    const timestamp = new Date();
-    const dbRow = {
-      id: 'comment-123',
-      content: 'Sebuah komentar',
-      username: 'johndoe',
-      created_at: timestamp,
-      is_delete: false,
-    };
+  describe('mapCommentListToDomain', () => {
+    it('should correctly map data list to array of Comment domain entity', () => {
+      const row1 = createRawComment({ id: 'comment-001' });
+      const row2 = createRawComment({ id: 'comment-002' });
 
-    const comment = CommentMapper.toEntity(dbRow);
-    expect(comment).toStrictEqual(new Comment({
-      id: dbRow.id,
-      content: dbRow.content,
-      username: dbRow.username,
-      date: dbRow.created_at,
-      isDelete: dbRow.is_delete,
-    }));
+      const expectedComments = [
+        new Comment({
+          id: row1.id,
+          content: row1.content,
+          username: row1.username,
+          date: row1.created_at,
+          isDelete: row1.is_delete,
+        }),
+        new Comment({
+          id: row2.id,
+          content: row2.content,
+          username: row2.username,
+          date: row2.created_at,
+          isDelete: row2.is_delete,
+        }),
+      ];
+
+      const result = CommentMapper.mapCommentListToDomain([row1, row2]);
+      expect(result).toStrictEqual(expectedComments);
+    });
   });
 });
