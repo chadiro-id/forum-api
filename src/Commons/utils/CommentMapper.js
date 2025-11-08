d260aca827511da9220f5146316ab4873d76444f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 16:08:54 +0700

    Add added comment and comment list mapper

--- src/Commons/utils/CommentMapper.js
+++ src/Commons/utils/CommentMapper.js
@@ -1,27 +1,47 @@
+const AddedComment = require('../../Domains/comments/entities/AddedComment');
 const Comment = require('../../Domains/comments/entities/Comment');
 
 class CommentMapper {
   /**
-   * Menerjemahkan data hasil SELECT database
-   * @param {{
+   * Translate result data from database INSERT
+   * @param {{ id: string, content: string, owner_id: string }} row Persistence Model
+   * @returns Domain Entity
+   */
+  static mapAddedCommentToDomain({
+    id, content, owner_id
+  }) {
+    return new AddedComment({
+      id,
+      content,
+      owner: owner_id,
+    });
+  }
+
+  /**
+   * Translate result data from database SELECT
+   * @param {Array<{
    *   id: string,
    *   content: string,
    *   username: string,
    *   created_at: Date,
    *   is_delete: boolean,
-   * }} row Persistence
-   * @returns {Comment} Domain Entity
+   * }>} rows Persistence Model
+   * @returns Domain Entity
    */
-  static toEntity({
-    id, content, username, created_at, is_delete,
-  }) {
-    return new Comment({
+  static mapCommentListToDomain(rows) {
+    return rows.map(({
+      id,
+      content,
+      username,
+      created_at,
+      is_delete,
+    }) => new Comment({
       id,
       content,
       username,
       date: created_at,
       isDelete: is_delete,
-    });
+    }));
   }
 }
 
