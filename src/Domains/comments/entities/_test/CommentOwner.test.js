bc2630da7f31e11df1f58c1b96c97f53a6f8622d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 15:56:45 +0700

    Describe negative scenario

--- src/Domains/comments/entities/_test/CommentOwner.test.js
+++ src/Domains/comments/entities/_test/CommentOwner.test.js
@@ -7,4 +7,21 @@ describe('CommentOwner', () => {
     const { owner } = new CommentOwner(payload);
     expect(owner).toBe(payload.owner);
   });
+
+  it('should throw error when payload not contain needed property', () => {
+    const missingOwner = {};
+    const emptyOwner = { owner: '' };
+
+    expect(() => new CommentOwner(missingOwner))
+      .toThrow('COMMENT_OWNER.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new CommentOwner(emptyOwner))
+      .toThrow('COMMENT_OWNER.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload property does not meet data type specification', () => {
+    const ownerNotString = { owner: 123 };
+
+    expect(() => new CommentOwner(ownerNotString))
+      .toThrow('COMMENT_OWNER.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
 });
