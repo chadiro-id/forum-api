b1c1b6d460fa489f5b33b6a60bfd968a7be4390c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 15:23:19 +0700

    Remove reply deletion verification

--- src/Applications/use_case/replies/DeleteReplyUseCase.js
+++ src/Applications/use_case/replies/DeleteReplyUseCase.js
@@ -9,23 +9,15 @@ class DeleteReplyUseCase {
     const { threadId, commentId, replyId, owner } = new DeleteReply(payload);
 
     const reply = await this._replyRepository.getReplyForDeletion(replyId, commentId, threadId);
-    this._verifyReplyDeletion(reply);
+    if (reply === null) {
+      throw new Error('DELETE_REPLY_USE_CASE.REPLY_NOT_EXIST');
+    }
 
     if (reply.owner !== owner) {
       throw new Error('DELETE_REPLY_USE_CASE.OWNER_NOT_MATCH');
     }
 
     await this._replyRepository.softDeleteReplyById(replyId);
-  }
-
-  _verifyReplyDeletion(reply) {
-    if (reply === null) {
-      throw new Error('DELETE_REPLY_USE_CASE.REPLY_NOT_EXIST');
-    }
-
-    if (!reply.owner || typeof reply.owner !== 'string') {
-      throw new Error('DELETE_REPLY_USE_CASE.REPLY_OWNER_MUST_NON_EMPTY_STRING');
-    }
   }
 }
 
