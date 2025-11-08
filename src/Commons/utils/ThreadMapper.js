91338d492f396400f8b6ccfccad85185b15eb299
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-08 16:45:27 +0700

    Add thread details mapper

--- src/Commons/utils/ThreadMapper.js
+++ src/Commons/utils/ThreadMapper.js
@@ -1,4 +1,5 @@
 const AddedThread = require('../../Domains/threads/entities/AddedThread');
+const ThreadDetails = require('../../Domains/threads/entities/ThreadDetails');
 
 class ThreadMapper {
   /**
@@ -13,6 +14,33 @@ class ThreadMapper {
       id,
       title,
       owner: owner_id,
+    });
+  }
+
+  /**
+   * Translate result data from database SELECT
+   * @param {{
+   *   id: string,
+   *   title: string,
+   *   body: string,
+   *   username: string,
+   *   created_at: Date
+   * }} row Persistence Model
+   * @returns Domain Entity
+   */
+  static mapThreadDetailsToDomain({
+    id,
+    title,
+    body,
+    username,
+    created_at
+  }) {
+    return new ThreadDetails({
+      id,
+      title,
+      body,
+      username,
+      date: created_at,
     });
   }
 }
