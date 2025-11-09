78eaccc6e2765350271a3f439d10ee3b26f5724a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 01:52:16 +0700

    Fix: NewThread entity owner property renamed

--- src/Domains/threads/entities/_test/NewThread.test.js
+++ src/Domains/threads/entities/_test/NewThread.test.js
@@ -4,7 +4,7 @@ describe('NewThread Entity', () => {
   const dummyPayload = {
     title: 'Something thread title',
     body: 'Something thread body',
-    owner: 'user-123',
+    userId: 'user-123',
   };
 
   describe('Bad Payload', () => {
@@ -13,37 +13,37 @@ describe('NewThread Entity', () => {
       delete missingTitle.title;
       const missingBody = { ...dummyPayload };
       delete missingBody.body;
-      const missingOwner = { ...dummyPayload };
-      delete missingOwner.owner;
+      const missingUserId = { ...dummyPayload };
+      delete missingUserId.userId;
 
       const emptyTitle = { ...dummyPayload, title: '' };
       const emptyBody = { ...dummyPayload, body: '' };
-      const emptyOwner = { ...dummyPayload, owner: '' };
+      const emptyUserId = { ...dummyPayload, userId: '' };
 
       expect(() => new NewThread(missingTitle))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
       expect(() => new NewThread(missingBody))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-      expect(() => new NewThread(missingOwner))
+      expect(() => new NewThread(missingUserId))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
       expect(() => new NewThread(emptyTitle))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
       expect(() => new NewThread(emptyBody))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
-      expect(() => new NewThread(emptyOwner))
+      expect(() => new NewThread(emptyUserId))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
     });
 
     it('should throw error when payload property does not meet data type specification', () => {
       const titleNotString = { ...dummyPayload, title: 123 };
       const bodyNotString = { ...dummyPayload, body: true };
-      const ownerNotString = { ...dummyPayload, owner: ['user-123'] };
+      const userIdNotString = { ...dummyPayload, userId: ['user-123'] };
 
       expect(() => new NewThread(titleNotString))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
       expect(() => new NewThread(bodyNotString))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
-      expect(() => new NewThread(ownerNotString))
+      expect(() => new NewThread(userIdNotString))
         .toThrow('NEW_THREAD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
     });
 
@@ -59,11 +59,11 @@ describe('NewThread Entity', () => {
     it('should correctly create the entity', () => {
       const payload = { ...dummyPayload };
 
-      const { title, body, owner } = new NewThread(payload);
+      const { title, body, userId } = new NewThread(payload);
 
       expect(title).toEqual(payload.title);
       expect(body).toEqual(payload.body);
-      expect(owner).toEqual(payload.owner);
+      expect(userId).toEqual(payload.userId);
     });
   });
 });
