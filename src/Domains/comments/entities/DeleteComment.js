c04a216f1f8f78a41f492df1c8aaad3a02a2f612
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 02:30:49 +0700

    Rename owner property to userId

--- src/Domains/comments/entities/DeleteComment.js
+++ src/Domains/comments/entities/DeleteComment.js
@@ -1,23 +1,30 @@
 class DeleteComment {
+  /**
+   * Delete Comment Entity constructor
+   * @param {Object} payload
+   * @property {string} threadId
+   * @property {string} commentId
+   * @property {string} userId
+   */
   constructor(payload) {
     this._verifyPayload(payload);
 
     this.threadId = payload.threadId;
     this.commentId = payload.commentId;
-    this.owner = payload.owner;
+    this.userId = payload.userId;
   }
 
   _verifyPayload(payload) {
-    const { threadId, commentId, owner } = payload;
+    const { threadId, commentId, userId } = payload;
 
-    if (!threadId || !commentId || !owner) {
+    if (!threadId || !commentId || !userId) {
       throw new Error('DELETE_COMMENT.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
     if (
       typeof threadId !== 'string'
       || typeof commentId !== 'string'
-      || typeof owner !== 'string'
+      || typeof userId !== 'string'
     ) {
       throw new Error('DELETE_COMMENT.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
