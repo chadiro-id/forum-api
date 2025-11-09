027a2cfa5bbcafc61145085c0b755da1742921f3
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-09 10:24:49 +0700

    Update error message to be consistent

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -7,21 +7,20 @@ const usersUseCaseError = {
   'ADD_USER_USE_CASE.USERNAME_NOT_AVAILABLE': new InvariantError('username tidak tersedia'),
 };
 const authenticationsUseCaseError = {
-  'ADD_AUTHENTICATION_USE_CASE.USER_NOT_EXIST': new InvariantError('user tidak ada, username tidak ditemukan'),
+  'ADD_AUTHENTICATION_USE_CASE.USER_NOT_FOUND': new InvariantError('user tidak ada, username tidak ditemukan'),
   'ADD_AUTHENTICATION_USE_CASE.PASSWORD_NOT_MATCH': new AuthenticationError('kredensial yang Anda masukkan salah'),
   'PUT_AUTHENTICATION_USE_CASE.REFRESH_TOKEN_NOT_FOUND': new InvariantError('refresh token tidak ditemukan di database'),
   'PUT_AUTHENTICATION_USE_CASE.REFRESH_TOKEN_NOT_VALID': new InvariantError('refresh token tidak valid'),
   'DELETE_AUTHENTICATION_USE_CASE.REFRESH_TOKEN_NOT_FOUND': new InvariantError('refresh token tidak ditemukan di database'),
 };
-const threadsUseCaseError = {};
 const commentsUseCaseError = {
   'ADD_COMMENT_USE_CASE.THREAD_NOT_FOUND': new NotFoundError('Thread tidak ditemukan'),
-  'DELETE_COMMENT_USE_CASE.COMMENT_NOT_EXIST': new NotFoundError('Komentar tidak ditemukan'),
+  'DELETE_COMMENT_USE_CASE.COMMENT_NOT_FOUND': new NotFoundError('Komentar tidak ditemukan'),
   'DELETE_COMMENT_USE_CASE.OWNER_NOT_MATCH': new AuthorizationError('Pengguna tidak memiliki hak akses'),
 };
 const repliesUseCaseError = {
-  'ADD_REPLY_USE_CASE.COMMENT_NOT_EXIST': new NotFoundError('Komentar tidak ditemukan'),
-  'DELETE_REPLY_USE_CASE.REPLY_NOT_EXIST': new NotFoundError('Balasan tidak ditemukan'),
+  'ADD_REPLY_USE_CASE.COMMENT_NOT_FOUND': new NotFoundError('Komentar tidak ditemukan'),
+  'DELETE_REPLY_USE_CASE.REPLY_NOT_FOUND': new NotFoundError('Balasan tidak ditemukan'),
   'DELETE_REPLY_USE_CASE.OWNER_NOT_MATCH': new AuthorizationError('Pengguna tidak memiliki hak akses'),
 };
 
@@ -29,7 +28,6 @@ const directories = Object.assign(
   {},
   usersUseCaseError,
   authenticationsUseCaseError,
-  threadsUseCaseError,
   commentsUseCaseError,
   repliesUseCaseError
 );
