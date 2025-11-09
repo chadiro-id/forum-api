8bf33126645ce6703c5a6197f197257a3e2ad013
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-09 16:33:16 +0700

    Remove unused assertion helper

--- tests/helper/assertionsHelper.js
+++ tests/helper/assertionsHelper.js
@@ -20,50 +20,6 @@ const assertDBError = async (promise) => {
   await expect(promise).rejects.not.toThrow(ClientError);
 };
 
-const assertHttpResponseError = (
-  response, statusCode, { status = 'fail', message = 'any' } = {}
-) => {
-  expect(response.statusCode).toBe(statusCode);
-
-  const resJson = JSON.parse(response.payload);
-
-  if (status) {
-    expect(resJson.status).toEqual(status);
-  }
-
-  if (message === 'any') {
-    expect(resJson.message).toEqual(expect.any(String));
-    expect(resJson.message.trim()).not.toBe('');
-  } else {
-    expect(resJson.message).toEqual(
-      expect.stringContaining(message)
-    );
-  }
-};
-
-const expectCommentFromResponse = (comment, expectedSource) => {
-  const expectedContent = expectedSource.is_delete
-    ? '**komentar telah dihapus**'
-    : expectedSource.content;
-
-  expect(comment.id).toEqual(expectedSource.id);
-  expect(comment.content).toEqual(expectedContent);
-  expect(comment.username).toEqual(expectedSource.username);
-  expect(Date.parse(comment.date)).not.toBeNaN();
-  expect(comment.replies).toHaveLength(expectedSource.replies.length);
-};
-
-const expectReplyFromResponse = (reply, expectedSource) => {
-  const expectedContent = expectedSource.is_delete
-    ? '**balasan telah dihapus**'
-    : expectedSource.content;
-
-  expect(reply.id).toEqual(expectedSource.id);
-  expect(reply.content).toEqual(expectedContent);
-  expect(reply.username).toEqual(expectedSource.username);
-  expect(Date.parse(reply.date)).not.toBeNaN();
-};
-
 const expectCommentFromRepository = (comment, expectedSource) => {
   const expectedContent = expectedSource.is_delete
     ? '**komentar telah dihapus**'
@@ -91,9 +47,6 @@ const expectReplyFromRepository = (reply, expectedSource) => {
 module.exports = {
   assertQueryCalled,
   assertDBError,
-  assertHttpResponseError,
-  expectCommentFromResponse,
-  expectReplyFromResponse,
   expectCommentFromRepository,
   expectReplyFromRepository,
 };
