966b683eae2124dec04b3101707dd6ebaa5210dd
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 00:21:32 +0700

    Adding JSdoc on constructor

--- src/Domains/replies/entities/Reply.js
+++ src/Domains/replies/entities/Reply.js
@@ -1,4 +1,14 @@
 class Reply {
+  /**
+   * Reply Entity Constructor
+   * @param {Object} payload
+   * @property {string} id
+   * @property {string} commentId
+   * @property {string} username
+   * @property {string} content
+   * @property {Date} date
+   * @property {boolean} isDelete
+   */
   constructor(payload) {
     this._validatePayload(payload);
 
