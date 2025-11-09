2da40c36163129192b79658aa11db023bdb23e33
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 02:15:20 +0700

    Rename owner property

--- src/Domains/comments/entities/NewComment.js
+++ src/Domains/comments/entities/NewComment.js
@@ -1,23 +1,30 @@
 class NewComment {
+  /**
+   * New Comment Entity constructor
+   * @param {Object} payload
+   * @property {string} threadId
+   * @property {string} content
+   * @property {string} userId
+   */
   constructor(payload) {
     this._verifyPayload(payload);
 
     this.threadId = payload.threadId;
     this.content = payload.content;
-    this.owner = payload.owner;
+    this.userId = payload.userId;
   }
 
   _verifyPayload(payload) {
-    const { threadId, content, owner } = payload;
+    const { threadId, content, userId } = payload;
 
-    if (!threadId || !content || !owner) {
+    if (!threadId || !content || !userId) {
       throw new Error('NEW_COMMENT.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
     if (
       typeof threadId !== 'string'
       || typeof content !== 'string'
-      || typeof owner !== 'string'
+      || typeof userId !== 'string'
     ) {
       throw new Error('NEW_COMMENT.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
