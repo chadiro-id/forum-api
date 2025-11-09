ce7d4acb9a499594a0e87ebf22a857706128219e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-09 08:29:55 +0700

    Provide payload verification

--- src/Domains/authentications/entities/AuthRefreshPayload.js
+++ src/Domains/authentications/entities/AuthRefreshPayload.js
@@ -1,6 +1,18 @@
 class AuthRefreshPayload {
   constructor(payload) {
+    this._verifyPayload(payload);
+
     this.refreshToken = payload.refreshToken;
+  }
+
+  _verifyPayload({ refreshToken }) {
+    if (!refreshToken) {
+      throw new Error('AUTH_REFRESH_PAYLOAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof refreshToken !== 'string') {
+      throw new Error('AUTH_REFRESH_PAYLOAD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
   }
 }
 
