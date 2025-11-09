360376c7f6a302f41bcb808ae6f7322471fa64e9
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-09 08:21:45 +0700

    Add bad payload test cases

--- src/Domains/authentications/entities/_test/AuthRefreshPayload.test.js
+++ src/Domains/authentications/entities/_test/AuthRefreshPayload.test.js
@@ -7,4 +7,21 @@ describe('AuthRefreshPayload Entity', () => {
     const { refreshToken } = new AuthRefreshPayload(payload);
     expect(refreshToken).toStrictEqual(payload.refreshToken);
   });
+
+  it('should throw error when payload not contain needed property', () => {
+    const missingRefreshToken = {};
+    const emptyRefreshToken = { refreshToken: '' };
+
+    expect(() => new AuthRefreshPayload(missingRefreshToken))
+      .rejects.toThrow('AUTH_REFRESH_PAYLOAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new AuthRefreshPayload(emptyRefreshToken))
+      .rejects.toThrow('AUTH_REFRESH_PAYLOAD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload property does not meet data type specification', () => {
+    const refreshTokenNotString = { refreshToken: 123 };
+
+    expect(() => new AuthRefreshPayload(refreshTokenNotString))
+      .rejects.toThrow('AUTH_REFRESH_PAYLOAD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
 });
