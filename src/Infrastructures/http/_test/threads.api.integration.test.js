7b5a188e045a798a98e64c2782234de45581dd13
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-09 15:55:05 +0700

    Update detail thread assertions

--- src/Infrastructures/http/_test/threads.api.integration.test.js
+++ src/Infrastructures/http/_test/threads.api.integration.test.js
@@ -1,10 +1,6 @@
 const serverTest = require('../../../../tests/helper/ServerTestHelper');
 const pgTest = require('../../../../tests/helper/postgres');
 const { createAuthToken } = require('../../../../tests/helper/authenticationHelper');
-const {
-  expectCommentFromResponse,
-  expectReplyFromResponse,
-} = require('../../../../tests/helper/assertionsHelper');
 
 beforeAll(async () => {
   await serverTest.init();
@@ -48,17 +44,19 @@ describe('[Integration] Threads Endpoints', () => {
       };
 
       const response = await serverTest.post('/threads', options);
-
-      const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toBe(201);
-      expect(responseJson.status).toBe('success');
-      expect(responseJson.data.addedThread).toEqual(
-        expect.objectContaining({
-          id: expect.stringContaining('thread-'),
-          title: dummyPayload.title,
-          owner: user.id,
-        })
-      );
+
+      const resJson = JSON.parse(response.payload);
+      expect(resJson).toStrictEqual({
+        status: 'success',
+        data: {
+          addedThread: {
+            id: expect.stringMatching(/^thread-[A-Za-z0-9_-]{21}$/),
+            title: dummyPayload.title,
+            owner: user.id,
+          }
+        }
+      });
     });
 
     it('should response 401 when request with no authentication', async () => {
@@ -141,23 +139,25 @@ describe('[Integration] Threads Endpoints', () => {
     });
 
     it('should response 200 and detail thread', async () => {
+      const expectedResJson = {
+        status: 'success',
+        data: {
+          thread: {
+            id: thread.id,
+            title: thread.title,
+            body: thread.body,
+            username: user.username,
+            date: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2})$/),
+            comments: [],
+          },
+        },
+      };
+
       const response = await serverTest.get(`/threads/${thread.id}`);
-
-      const responseJson = JSON.parse(response.payload);
-
       expect(response.statusCode).toBe(200);
-      expect(responseJson.status).toEqual('success');
-      expect(responseJson.data.thread).toBeDefined();
 
-      const detailThread = responseJson.data.thread;
-      expect(detailThread).toMatchObject({
-        id: thread.id,
-        title: thread.title,
-        body: thread.body,
-        username: user.username,
-      });
-      expect(Date.parse(detailThread.date)).not.toBeNaN();
-      expect(detailThread.comments).toHaveLength(0);
+      const resJson = JSON.parse(response.payload);
+      expect(resJson).toStrictEqual(expectedResJson);
     });
 
     it('should handle all comments including soft-deleted ones', async () => {
@@ -165,20 +165,26 @@ describe('[Integration] Threads Endpoints', () => {
       const commentB = await pgTest.comments.add({ id: 'comment-002', thread_id: thread.id, owner_id: user.id, is_delete: true });
 
       const response = await serverTest.get(`/threads/${thread.id}`);
-
-      const responseJson = JSON.parse(response.payload);
-
       expect(response.statusCode).toBe(200);
-      expect(responseJson.status).toEqual('success');
-      expect(responseJson.data.thread).toBeDefined();
 
-      const comments = responseJson.data.thread.comments;
-      expect(comments).toHaveLength(2);
-
-      const c1 = comments.find((c) => c.id === commentA.id);
-      const c2 = comments.find((c) => c.id === commentB.id);
-      expectCommentFromResponse(c1, { ...commentA, username: user.username, replies: [] });
-      expectCommentFromResponse(c2, { ...commentB, username: user.username, replies: [] });
+      const resJson = JSON.parse(response.payload);
+      const comments = resJson.data.thread.comments;
+      expect(comments).toStrictEqual([
+        {
+          id: commentA.id,
+          content: commentA.content,
+          username: user.username,
+          date: commentA.created_at.toISOString(),
+          replies: [],
+        },
+        {
+          id: commentB.id,
+          content: '**komentar telah dihapus**',
+          username: user.username,
+          date: commentB.created_at.toISOString(),
+          replies: [],
+        }
+      ]);
     });
 
     it('should handle all replies including soft-deleted ones', async () => {
@@ -187,23 +193,28 @@ describe('[Integration] Threads Endpoints', () => {
       const replyB = await pgTest.replies.add({ id: 'reply-002', comment_id: comment.id, owner_id: user.id, is_delete: true });
 
       const response = await serverTest.get(`/threads/${thread.id}`);
-
-      const responseJson = JSON.parse(response.payload);
-
       expect(response.statusCode).toBe(200);
-      expect(responseJson.status).toEqual('success');
-      expect(responseJson.data.thread).toBeDefined();
 
-      const comments = responseJson.data.thread.comments;
+      const resJson = JSON.parse(response.payload);
+
+      const comments = resJson.data.thread.comments;
       expect(comments).toHaveLength(1);
 
       const replies = comments[0].replies;
-      expect(replies).toHaveLength(2);
-
-      const r1 = replies.find((r) => r.id === replyA.id);
-      const r2 = replies.find((r) => r.id === replyB.id);
-      expectReplyFromResponse(r1, { ...replyA, username: user.username });
-      expectReplyFromResponse(r2, { ...replyB, username: user.username });
+      expect(replies).toStrictEqual([
+        {
+          id: replyA.id,
+          content: replyA.content,
+          username: user.username,
+          date: replyA.created_at.toISOString(),
+        },
+        {
+          id: replyB.id,
+          content: '**balasan telah dihapus**',
+          username: user.username,
+          date: replyB.created_at.toISOString(),
+        },
+      ]);
     });
   });
 });
