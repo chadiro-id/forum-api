3c7630c166b21b79298ef2ee3b46ace8fa472799
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-09 08:36:14 +0700

    Update payload verification using entity

--- src/Applications/use_case/authentications/DeleteAuthenticationUseCase.js
+++ src/Applications/use_case/authentications/DeleteAuthenticationUseCase.js
@@ -1,12 +1,12 @@
+const AuthRefreshPayload = require('../../../Domains/authentications/entities/AuthRefreshPayload');
+
 class DeleteAuthenticationUseCase {
   constructor({ authenticationRepository }) {
     this._authenticationRepository = authenticationRepository;
   }
 
   async execute(payload) {
-    this._verifyPayload(payload);
-
-    const { refreshToken } = payload;
+    const { refreshToken } = new AuthRefreshPayload(payload);
 
     const isTokenExist = await this._authenticationRepository.isTokenExist(refreshToken);
     if (!isTokenExist) {
@@ -14,18 +14,6 @@ class DeleteAuthenticationUseCase {
     }
 
     await this._authenticationRepository.deleteToken(refreshToken);
-  }
-
-  _verifyPayload(payload) {
-    const { refreshToken } = payload;
-
-    if (!refreshToken) {
-      throw new Error('DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_CONTAIN_REFRESH_TOKEN');
-    }
-
-    if (typeof refreshToken !== 'string') {
-      throw new Error('DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
-    }
   }
 }
 
