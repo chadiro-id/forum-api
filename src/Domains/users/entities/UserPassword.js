3bdef0ebfe395a8650ae7d950f9568cbe19cdf2d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 20:59:03 +0700

    Provide payload verification

--- src/Domains/users/entities/UserPassword.js
+++ src/Domains/users/entities/UserPassword.js
@@ -5,7 +5,19 @@ class UserPassword {
    * @property {string} password
    */
   constructor(payload) {
+    this._verifyPayload(payload);
+
     this.password = payload.password;
+  }
+
+  _verifyPayload({ password }) {
+    if (!password) {
+      throw new Error('USER_PASSWORD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof password !== 'string') {
+      throw new Error('USER_PASSWORD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
   }
 }
 
