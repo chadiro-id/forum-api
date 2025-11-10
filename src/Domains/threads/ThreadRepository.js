ae3fa818aec1b6999332c616a650378a3128c814
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 12:35:57 +0700

    Replace get thread by id with get thread details

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -3,7 +3,7 @@ class ThreadRepository {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
-  async getThreadById(_id) {
+  async getThreadDetails(_id) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
