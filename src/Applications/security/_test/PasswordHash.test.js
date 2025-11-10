838fc9739d94dc3f05497dd212e40b0544f91dcb
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 14:46:37 +0700

    Refactor test cases

--- src/Applications/security/_test/PasswordHash.test.js
+++ src/Applications/security/_test/PasswordHash.test.js
@@ -1,12 +1,14 @@
 const PasswordHash = require('../PasswordHash');
 
 describe('PasswordHash', () => {
-  it('should throw error with properly message when abstract method invoked', async () => {
-    const passwordHash = new PasswordHash();
+  const passwordHash = new PasswordHash();
+  const methods = [
+    passwordHash.hash,
+    passwordHash.comparePassword,
+  ];
 
-    await expect(passwordHash.hash('some_password'))
-      .rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
-    await expect(passwordHash.comparePassword('plain', 'encrypted'))
+  test.each(methods)('should throw error when abstract behavior invoked %p', async (fn) => {
+    await expect(fn())
       .rejects.toThrow('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
   });
 });
