2c2b06097b489820d08efddc14b00431ebe55791
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 20:55:19 +0700

    Add bad payload test cases

--- src/Domains/users/entities/_test/UserPassword.test.js
+++ src/Domains/users/entities/_test/UserPassword.test.js
@@ -7,4 +7,21 @@ describe('UserPassword Entity', () => {
     const { password } = new UserPassword(payload);
     expect(password).toEqual(payload.password);
   });
+
+  it('should throw error when payload not contain needed property', () => {
+    const missingPassword = {};
+    const emptyPassword = { password: '' };
+
+    expect(() => new UserPassword(missingPassword))
+      .toThrow('USER_PASSWORD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new UserPassword(emptyPassword))
+      .toThrow('USER_PASSWORD.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload property does not meet data type specification', () => {
+    const passwordNotString = { password: 123 };
+
+    expect(() => new UserPassword(passwordNotString))
+      .toThrow('USER_PASSWORD.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
 });
