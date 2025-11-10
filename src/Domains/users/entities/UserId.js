27ad68d30326e36e305e68b4c0404e967001852e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-11-10 20:43:51 +0700

    Provide payload validation

--- src/Domains/users/entities/UserId.js
+++ src/Domains/users/entities/UserId.js
@@ -5,7 +5,19 @@ class UserId {
    * @property {string} id
    */
   constructor(payload) {
+    this._verifyPayload(payload);
+
     this.id = payload.id;
+  }
+
+  _verifyPayload({ id }) {
+    if (!id) {
+      throw new Error('USER_ID.PAYLOAD_NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof id !== 'string') {
+      throw new Error('USER_ID.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
   }
 }
 
